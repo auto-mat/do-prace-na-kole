e64c10719285eddfe438ce4fb4993bdd4c128f0a
Petr Dlouhý <petr.dlouhy@email.cz>
2013-02-28 11:34:35 +0100

    detaching forms to forms.py

---
+++ dpnk/forms.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+from smart_selects.form_fields import ChainedModelChoiceField, GroupedModelSelect
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms, http
+# Registration imports
+import registration.forms
+from models import UserProfile, Company, Subsidiary, Team
+
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('firstname', 'surname', 'telephone')
+    
+
+class RegistrationFormDPNK(registration.forms.RegistrationForm):
+    required_css_class = 'required'
+    
+    firstname = forms.CharField(
+        label="Jméno",
+        max_length=30,
+        required=True)
+    surname = forms.CharField(
+        label="Příjmení",
+        max_length=30,
+        required=True)
+    company = forms.ModelChoiceField(
+        label="Firma",
+        queryset=Company.objects.all(),
+        required=True)
+    subsidiary = ChainedModelChoiceField(
+        chain_field = "company",
+        app_name = "dpnk",
+        model_name = "Subsidiary",
+        model_field = "company",
+        show_all = False,
+        auto_choose = True,
+        label="Pobočka",
+        queryset=Team.objects.all(),
+        required=True)
+    team = ChainedModelChoiceField(
+        chain_field = "subsidiary",
+        app_name = "dpnk",
+        model_name = "Team",
+        model_field = "subsidiary",
+        show_all = False,
+        auto_choose = True,
+        label="Tým",
+        queryset=Team.objects.all(),
+        required=True)
+    team_password = forms.CharField(
+        label="Tajný kód týmu",
+        max_length=20)
+    distance = forms.IntegerField(
+        label="Vzdálenost z domova do práce vzdušnou čarou (v km)",
+        required=True)
+    t_shirt_size = forms.ChoiceField(
+        label="Velikost trička",
+        choices = UserProfile.TSHIRTSIZE,
+        initial = "L",
+        )
+
+    # -- Contacts
+    telephone = forms.CharField(
+        label="Telefon",
+        max_length=30)
+
+    def __init__(self, request=None, *args, **kwargs):
+        if request:
+            initial = kwargs.get('initial', {})
+            if request.GET.get('team_password', None):
+                initial['team_password'] = request.GET['team_password']
+            if request.GET.get('team', None):
+                initial['team'] = request.GET['team']
+            kwargs['initial']=initial
+
+        super(RegistrationFormDPNK, self).__init__(*args, **kwargs)
+        self.fields.keyOrder = [
+            'firstname',
+            'surname',
+            'company',
+            'subsidiary',
+            'team',
+            'team_password',
+            'distance',
+            't_shirt_size',
+            'email',
+            'telephone',
+            'username',
+            'password1',
+            'password2'
+            ]
+
+    def clean_team(self):
+        data = self.cleaned_data['team']
+        if len(UserProfile.objects.filter(team=data, active=True)) >= 5:
+            raise forms.ValidationError("Tento tým již má pět členů a je tedy plný")
+        return data
+
+    def clean_team_password(self):
+        data = self.cleaned_data['team_password']
+        try:
+            team = Team.objects.get(id=self.data['team'])
+        except (Team.DoesNotExist, ValueError):
+            raise forms.ValidationError("Neexistující nebo neplatný tým")
+        if data.strip().lower() != team.password.strip():
+            raise forms.ValidationError("Nesprávné heslo týmu")
+        return data
+
+    class Meta:
+        model = UserProfile
+
+class AutoRegistrationFormDPNK(RegistrationFormDPNK):
+
+    def clean_team_password(self):
+        return self.cleaned_data['team_password']
+
+class RegisterTeamForm(forms.ModelForm):
+    required_css_class = 'required'
+    error_css_class = 'error'
+
+    company = forms.ModelChoiceField(
+        label="Firma",
+        queryset=Company.objects.all(),
+        required=True)
+    subsidiary = ChainedModelChoiceField(
+        chain_field = "company",
+        app_name = "dpnk",
+        model_name = "Subsidiary",
+        model_field = "company",
+        show_all = False,
+        auto_choose = True,
+        label="Pobočka",
+        queryset=Team.objects.all(),
+        required=True)
+    subsidiary1 = GroupedModelSelect(
+        label = "Pobočka",
+        order_field = "company",
+        queryset=Subsidiary.objects.all(),
+        )
+    
+    class Meta:
+        model = Team
+        fields = ('name', 'company', 'subsidiary', 'subsidiary1', 'password')
--- dpnk/views.py
+++ dpnk/views.py
@@ -20,118 +20,19 @@
 # Standard library imports
 import time, random, httplib, urllib, hashlib, datetime
 # Django imports
-from django import forms, http
 from django.shortcuts import render_to_response, redirect
 import django.contrib.auth
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.core.mail import EmailMessage
 from django.db.models import Sum, Count
-from smart_selects.form_fields import ChainedModelChoiceField, GroupedModelSelect
 # Registration imports
-import registration.forms, registration.signals, registration.backends
+import registration.signals, registration.backends
 # Model imports
-from models import *
+from models import UserProfile, Voucher, Trip, Answer, Question
+from forms import RegistrationFormDPNK, RegisterTeamForm, AutoRegistrationFormDPNK
 # Local imports
 import util
-
-class RegistrationFormDPNK(registration.forms.RegistrationForm):
-    required_css_class = 'required'
-    
-    firstname = forms.CharField(
-        label="Jméno",
-        max_length=30,
-        required=True)
-    surname = forms.CharField(
-        label="Příjmení",
-        max_length=30,
-        required=True)
-    company = forms.ModelChoiceField(
-        label="Firma",
-        queryset=Company.objects.all(),
-        required=True)
-    subsidiary = ChainedModelChoiceField(
-        chain_field = "company",
-        app_name = "dpnk",
-        model_name = "Subsidiary",
-        model_field = "company",
-        show_all = False,
-        auto_choose = True,
-        label="Pobočka",
-        queryset=Team.objects.all(),
-        required=True)
-    team = ChainedModelChoiceField(
-        chain_field = "subsidiary",
-        app_name = "dpnk",
-        model_name = "Team",
-        model_field = "subsidiary",
-        show_all = False,
-        auto_choose = True,
-        label="Tým",
-        queryset=Team.objects.all(),
-        required=True)
-    team_password = forms.CharField(
-        label="Tajný kód týmu",
-        max_length=20)
-    distance = forms.IntegerField(
-        label="Vzdálenost z domova do práce vzdušnou čarou (v km)",
-        required=True)
-    t_shirt_size = forms.ChoiceField(
-        label="Velikost trička",
-        choices = UserProfile.TSHIRTSIZE,
-        initial = "L",
-        )
-
-    # -- Contacts
-    telephone = forms.CharField(
-        label="Telefon",
-        max_length=30)
-
-    def __init__(self, request=None, *args, **kwargs):
-        if request:
-            initial = kwargs.get('initial', {})
-            if request.GET.get('team_password', None):
-                initial['team_password'] = request.GET['team_password']
-            if request.GET.get('team', None):
-                initial['team'] = request.GET['team']
-            kwargs['initial']=initial
-
-        super(RegistrationFormDPNK, self).__init__(*args, **kwargs)
-        self.fields.keyOrder = [
-            'firstname',
-            'surname',
-            'company',
-            'subsidiary',
-            'team',
-            'team_password',
-            'distance',
-            't_shirt_size',
-            'email',
-            'telephone',
-            'username',
-            'password1',
-            'password2'
-            ]
-
-    def clean_team(self):
-        data = self.cleaned_data['team']
-        if len(UserProfile.objects.filter(team=data, active=True)) >= 5:
-            raise forms.ValidationError("Tento tým již má pět členů a je tedy plný")
-        return data
-
-    def clean_team_password(self):
-        data = self.cleaned_data['team_password']
-        try:
-            team = Team.objects.get(id=self.data['team'])
-        except (Team.DoesNotExist, ValueError):
-            raise forms.ValidationError("Neexistující nebo neplatný tým")
-        if data.strip().lower() != team.password.strip():
-            raise forms.ValidationError("Nesprávné heslo týmu")
-        return data
-
-    class Meta:
-        model = UserProfile
 
 def register(request, backend='registration.backends.simple.SimpleBackend',
              success_url=None, form_class=None,
@@ -158,10 +59,6 @@ def register(request, backend='registration.backends.simple.SimpleBackend',
                               {'form': form})
 
 
-class AutoRegistrationFormDPNK(RegistrationFormDPNK):
-
-    def clean_team_password(self):
-        return self.cleaned_data['team_password']
 
 # Temporary to allow mass XLS registrations
 def auto_register(request, backend='registration.backends.simple.SimpleBackend',
@@ -204,34 +101,6 @@ def create_profile(user, request, **kwargs):
                 distance = request.POST['distance']
                 ).save()
 registration.signals.user_registered.connect(create_profile)
-
-class RegisterTeamForm(forms.ModelForm):
-    required_css_class = 'required'
-    error_css_class = 'error'
-
-    company = forms.ModelChoiceField(
-        label="Firma",
-        queryset=Company.objects.all(),
-        required=True)
-    subsidiary = ChainedModelChoiceField(
-        chain_field = "company",
-        app_name = "dpnk",
-        model_name = "Subsidiary",
-        model_field = "company",
-        show_all = False,
-        auto_choose = True,
-        label="Pobočka",
-        queryset=Team.objects.all(),
-        required=True)
-    subsidiary1 = GroupedModelSelect(
-        label = "Pobočka",
-        order_field = "company",
-        queryset=Subsidiary.objects.all(),
-        )
-    
-    class Meta:
-        model = Team
-        fields = ('name', 'company', 'subsidiary', 'subsidiary1', 'password')
 
 def register_team(request):
     if request.method == 'POST':
@@ -543,13 +412,6 @@ def results(request, template):
             'user_count': user_count,
             'team_count': team_count,
             })
-
-class ProfileUpdateForm(forms.ModelForm):
-
-    class Meta:
-        model = UserProfile
-        fields = ('firstname', 'surname', 'telephone')
-    
 
 @login_required
 def update_profile(request):
