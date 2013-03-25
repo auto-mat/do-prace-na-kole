4b41c3463ab6dc69209f26e69980c8d3cd544719
Petr Dlouhý <petr.dlouhy@email.cz>
2013-03-26 00:18:47 +0100

    fix lost password email link

--- apps/dpnk/templatetags/dpnk_tags.py
+++ apps/dpnk/templatetags/dpnk_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 register = template.Library()
 
 from dpnk.wp_urls import wp_reverse
@@ -6,3 +7,7 @@ from dpnk.wp_urls import wp_reverse
 @register.simple_tag
 def wp_url(name):
     return wp_reverse(name)
+
+@register.simple_tag
+def site_url():
+    return settings.SITE_URL
