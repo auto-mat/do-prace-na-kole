06e3b30090e6ff80f899614a120e8b6090a76030
Petr Dlouhý <petr.dlouhy@email.cz>
2013-04-09 18:22:15 +0200

    don't count default distance

--- apps/dpnk/views.py
+++ apps/dpnk/views.py
@@ -432,16 +432,16 @@ def rides(request, template='registration/rides.html'):
                 try:
                     trip.distance_to = int(request.POST.get('distance_to-' + str(day), None))
                 except:
-                    trip.distance_to = None
+                    trip.distance_to = 0
             else:
-                trip.distance_to = None
+                trip.distance_to = 0
             if trip.trip_from:
                 try:
                     trip.distance_from = int(request.POST.get('distance_from-' + str(day), None))
                 except:
-                    trip.distance_to = None
+                    trip.distance_to = 0
             else:
-                trip.distance_from = None
+                trip.distance_from = 0
             trip.save()
 
     trips = {}
@@ -463,17 +463,13 @@ def rides(request, template='registration/rides.html'):
             trip_count += int(trips[d].trip_to) + int(trips[d].trip_from)
             if trips[d].distance_to:
                 distance += trips[d].distance_to
-            else:
-                distance += int(trips[d].trip_to) * profile.distance
             if trips[d].distance_from:
                 distance += trips[d].distance_from
-            else:
-                distance += int(trips[d].trip_from) * profile.distance
         else:
             cd['default_trip_to'] = False
             cd['default_trip_from'] = False
-            cd['default_distance_to'] = ""
-            cd['default_distance_from'] = ""
+            cd['default_distance_to'] = "0"
+            cd['default_distance_from'] = "0"
         cd['percentage'] = float(trip_count)/(2*(i+1))*100
         cd['percentage_str'] = "%.0f" % (cd['percentage'])
         cd['distance'] = distance
