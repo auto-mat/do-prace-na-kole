a5e8f8efb01179105a6b60ae469650caad878114
Petr Dlouhý <petr.dlouhy@email.cz>
2013-05-10 08:56:40 +0200

    Revert "decreasing max_show_all to 1000"
    This reverts commit e65fdebaecb8662a1fad03cbaa1cbe5ad7f20a07.

--- apps/dpnk/admin.py
+++ apps/dpnk/admin.py
@@ -176,7 +176,7 @@ class UserAdmin(EnhancedModelAdminMixin, NestedModelAdmin, UserAdmin):
     search_fields = ['first_name', 'last_name', 'username', 'email', 'userprofile__team__subsidiary__company__name',]
     list_filter = ['is_staff', 'is_superuser', 'is_active', 'userprofile__team__subsidiary__city', 'company_admin__company_admin_approved', 'userprofile__approved_for_team', 'userprofile__t_shirt_size', 'userprofile__team__subsidiary__city', PaymentFilter]
     readonly_fields = ['password']
-    list_max_show_all = 1000
+    list_max_show_all = 10000
 
     def userprofile__payment_type(self, obj):
        pay_type = "(None)"
@@ -228,7 +228,7 @@ class TeamAdmin(EnhancedModelAdminMixin, admin.ModelAdmin):
     list_display = ('name', 'subsidiary', 'subsidiary__city', 'subsidiary__company', 'coordinator', 'member_count', 'id', )
     search_fields = ['name', 'subsidiary__address_street', 'subsidiary__company__name', 'coordinator__user__first_name', 'coordinator__user__last_name']
     list_filter = ['subsidiary__city', 'member_count', CoordinatorFilter]
-    list_max_show_all = 1000
+    list_max_show_all = 10000
 
     readonly_fields = ['members', 'invitation_token', 'member_count']
     def members(self, obj):
