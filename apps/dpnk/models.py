84b4c305ca5f2601a66db13e1208646b8f9c03c3
Petr Dlouhý <petr.dlouhy@email.cz>
2013-05-12 14:25:18 +0200

    show rides count, if user doesn't have distance competition

--- apps/dpnk/models.py
+++ apps/dpnk/models.py
@@ -406,6 +406,12 @@ class UserProfile(models.Model):
     def has_distance_competition(self):
         return results.has_distance_competition(self)
 
+    def rides_count(self):
+        trips = Trip.objects.filter(user=self)
+        if trips.count() == 0:
+            return 0
+        return trips.aggregate(Sum('trip_from'))['trip_from__sum'] + trips.aggregate(Sum('trip_to'))['trip_to__sum']
+
     def get_frequency(self):
         return results.get_userprofile_frequency(self)
 
