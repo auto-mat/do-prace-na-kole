1ec28a4a81404a47ff58999a49f46043b7ea18c8
Petr Dlouhý <petr.dlouhy@email.cz>
2014-04-11 15:37:19 +0200

    use localeurl application to make possible to override language

--- apps/dpnk/wp_urls.py
+++ apps/dpnk/wp_urls.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from django.utils import translation
+
 urls = {
     # Admin
     'admin':                        "/django/admin/",
@@ -7,50 +9,50 @@ urls = {
     # Registrace
     'chci_slapat':                  "/chci-slapat/",
     'registrace':                   "/registrace/",
-    'pozvanky':                     '/registrace/pozvanky',
-    'team_invitation':              '/registrace/tym-pozvanka',
-    'zaslat_zadost_clenstvi':       "/registrace/zaslat-zadost-o-clenstvi-v-tymu",
-    'typ_platby':                   "/registrace/typ-platby",
-    'platba':                       "/registrace/platba",
-    'platba_uspesna':               "/registrace/platba-uspesna",
-    'platba_neuspesna':             "/registrace/platba-neuspesna",
+    'pozvanky':                     '/registrace/pozvanky/',
+    'team_invitation':              '/registrace/tym-pozvanka/',
+    'zaslat_zadost_clenstvi':       "/registrace/zaslat-zadost-o-clenstvi-v-tymu/",
+    'typ_platby':                   "/registrace/typ-platby/",
+    'platba':                       "/registrace/platba/",
+    'platba_uspesna':               "/registrace/platba-uspesna/",
+    'platba_neuspesna':             "/registrace/platba-neuspesna/",
 
     # Profil
-    "profil":                       "/profil",
+    "profil":                       "/profil/",
     "profil_pristup":               "",
-    'login':                        "/prihlasit",
-    'logout':                       "/odhlasit",
-    'zapomenute_heslo':             "/profil/zapomenute-heslo",
-    'zapomenute_heslo_odeslano':    "/profil/zapomenute-heslo-odeslano",
-    'zapomenute_heslo_dokonceno':   "/profil/zapomenute-heslo-dokonceno",
-    'zapomenute_heslo_zmena':       "/profil/zapomenute-heslo-zmena",
-    'zmena_hesla':                  "/profil/zmena-hesla",
-    'zmena_hesla_hotovo':           "/profil/zmena-hesla-hotovo",
-    "upravit_profil":               "/profil/upravit-profil",
-    'team_admin':                   "/profil/team-admin",
-    'vysledky':                     "/profil/vysledky",
-    'vysledky_souteze':             "/profil/vysledky_souteze",
+    'login':                        "/prihlasit/",
+    'logout':                       "/odhlasit/",
+    'zapomenute_heslo':             "/profil/zapomenute-heslo/",
+    'zapomenute_heslo_odeslano':    "/profil/zapomenute-heslo-odeslano/",
+    'zapomenute_heslo_dokonceno':   "/profil/zapomenute-heslo-dokonceno/",
+    'zapomenute_heslo_zmena':       "/profil/zapomenute-heslo-zmena/",
+    'zmena_hesla':                  "/profil/zmena-hesla/",
+    'zmena_hesla_hotovo':           "/profil/zmena-hesla-hotovo/",
+    "upravit_profil":               "/profil/upravit-profil/",
+    'team_admin':                   "/profil/team-admin/",
+    'vysledky':                     "/profil/vysledky/",
+    'vysledky_souteze':             "/profil/vysledky_souteze/",
     'dotaznik':                     "/django/admin/dotaznik/",
     'odpovedi':                     "/django/admin/odpovedi/",
     'dotaznik_odpovedi':            "/django/admin/dotaznik_odpovedi/",
     'otazky':                       "/django/admin/otazky/",
-    'otazka':                       "/profil/dotaznik",
-    'zmenit_triko':                 "/profil/zmenit-triko",
-    'zmenit_tym':                   "/profil/zmenit-tym",
-    'dalsi_clenove':                "/profil/dalsi-clenove",
-    'souteze':                      "/profil/souteze",
-    'balicek':                      "/profil/balicek",
+    'otazka':                       "/profil/dotaznik/",
+    'zmenit_triko':                 "/profil/zmenit-triko/",
+    'zmenit_tym':                   "/profil/zmenit-tym/",
+    'dalsi_clenove':                "/profil/dalsi-clenove/",
+    'souteze':                      "/profil/souteze/",
+    'balicek':                      "/profil/balicek/",
 
     # Company admin
-    'edit_company':                 "/fa/editovat_spolecnost",
-    'zadost_firemni_spravce':       "/fa/zadost",
-    'soutez':                       "/fa/soutez",
-    'company_admin':                "/fa",
+    'edit_company':                 "/fa/editovat_spolecnost/",
+    'zadost_firemni_spravce':       "/fa/zadost/",
+    'soutez':                       "/fa/soutez/",
+    'company_admin':                "/fa/",
 
     # Other
-    'bike_repair':                  "/cykloservis",
-    'pravidla':                     "/pravidla",
-    'firma':                        "/firma",
+    'bike_repair':                  "/cykloservis/",
+    'pravidla':                     "/pravidla/",
+    'firma':                        "/firma/",
 
     # Zastarale nebo odlozeno na pozdeji
     'kratke_vysledky':              "kratke_vysledky.html",
@@ -62,4 +64,7 @@ if hasattr(settings, 'TESTING_URLS') and settings.TESTING_URLS:
     urls = wp_urls_testing.urls
 
 def wp_reverse(name):
-    return urls[name]
+    prefix = ""
+    if not (hasattr(settings, 'TESTING_URLS') and settings.TESTING_URLS) and translation.get_language() != settings.LANGUAGE_CODE:
+        prefix = "/" + translation.get_language()
+    return prefix + urls[name]
