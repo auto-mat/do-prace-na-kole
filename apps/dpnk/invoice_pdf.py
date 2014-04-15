7039de73350b6a15431d4d0de7dccc8d89c6c619
Petr Dlouhý <petr.dlouhy@email.cz>
2014-04-15 18:03:14 +0200

    use InvoiceGenerator for invoice PDF

--- apps/dpnk/invoice_pdf.py
+++ apps/dpnk/invoice_pdf.py
@@ -1,102 +1,50 @@
 # -*- coding: utf-8 -*-
-
 import os
-import reportlab
-from reportlab.platypus import Paragraph, Image
-from reportlab.pdfgen.canvas import Canvas
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.units import cm
-from reportlab.lib.enums import TA_CENTER
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-import locale
+from InvoiceGenerator.api import Invoice, Item, Client, Provider, Creator
+from InvoiceGenerator.pdf import SimpleInvoice
 
 
 def make_invoice_sheet_pdf(outfile, invoice):
-    canvas = Canvas(outfile, pagesize=A4)
-
-    folder = '/usr/share/fonts/truetype/ttf-dejavu'
-    reportlab.rl_config.TTFSearchPath.append(folder)
-    pdfmetrics.registerFont(TTFont('DejaVu', 'DejaVuSans.ttf'))
-    pdfmetrics.registerFont(TTFont('DejaVuB', 'DejaVuSans-Bold.ttf'))
-
-    make_sheet(invoice, canvas)
-    canvas.showPage()
-
-    canvas.save()
-
-
-def make_sheet(invoice, canvas):
     DIR = os.path.dirname(__file__)
-    # CONFIGURATION
-    logo_file = os.path.join(DIR, "static/img/logo.jpg")
-    stamp_file = os.path.join(DIR, "static/img/stamp.png")
-    # END OF CONFIGURATION
 
-    # STYLES
-    styles = getSampleStyleSheet()
-    styles['Normal'].fontName = 'DejaVu'
-    styles['Normal'].fontSize = 10
-    styles['Heading1'].fontName = 'DejaVuB'
-    styles['Heading1'].fontSize = 12
-    styles['Heading1'].alignment = TA_CENTER
-    styles.add(ParagraphStyle(name='Indented', leftIndent=290))
-    styles['Indented'].fontName = 'DejaVu'
+    client = Client(
+        invoice.company.name,
+        address=u"%s %s" % (invoice.company.address_street, invoice.company.address_street_number),
+        zip=invoice.company.address_psc,
+        city=invoice.company.address_city,
+        ir=invoice.company.ico,
+        )
 
-    # START OF THE DOCUMENT
-    im = Image(logo_file, 3.98*cm, 1.5*cm)
-    im.drawOn(canvas, 11*cm, 25*cm)
+    provider = Provider(
+        u"Auto*Mat, o.s.",
+        email=u"lucie.mullerova@auto-mat.cz",
+        address=u"Lublaňská 398/18",
+        zip=u"120 00",
+        city=u"Praha 2",
+        bank_name=u"Poštovní spořitelna",
+        bank_account='217 359 444 / 0300',
+        vat_id=u"CZ22670319",
+        ir=u"22670319",
+        phone=u"737 563 750",
+        note=u"""Sdružení Auto*Mat bylo zaregistrováno u MV ČR pod č. VS/1-1/68 776/07-R.
+U Městského soudu v Praze jsme vedeni pod spisovou značkou L 18119.
+Společně s vámi tvoříme město, ve kterém chceme žít. www.auto-mat.cz
+""",
+        )
 
-    canvas.setFont('DejaVuB', 20)
-    canvas.drawString(3*cm, 26*cm, u"FAKTURA %s/%s" % (invoice.sequence_number, invoice.exposure_date.year))
-    canvas.drawString(3*cm, 25*cm, u"Daňový doklad")
+    creator = Creator(
+        u'Lucie Mullerová',
+        stamp_filename=os.path.join(DIR, "static/img/stamp.png"),
+        )
 
-    canvas.setFont('DejaVuB', 10)
-    canvas.drawString(3*cm, 23.5*cm, u"OBJEDNATEL")
-    canvas.drawString(3*cm, 22.5*cm, invoice.company.name)
-    canvas.setFont('DejaVu', 10)
-    canvas.drawString(3*cm, 22*cm, u"%s %s" % (invoice.company.address_street, invoice.company.address_street_number))
-    canvas.drawString(3*cm, 21.5*cm, invoice.company.address_city)
-    canvas.drawString(3*cm, 21*cm, u"PSČ %s" % invoice.company.address_psc)
-    canvas.drawString(3*cm, 20.5*cm, u"IČO: %s" % invoice.company.ico)
+    invoice_gen = Invoice(client, provider, creator)
+    invoice_gen.title = u"Faktura %s/%s" % (invoice.sequence_number, invoice.exposure_date.year)
+    invoice_gen.variable_symbol = "%s%03d" % (invoice.exposure_date.year, invoice.sequence_number)
+    invoice_gen.date = invoice.exposure_date
 
-    canvas.setFont('DejaVuB', 10)
-    canvas.drawString(10*cm, 23.5*cm, u"DODAVATEL")
-    canvas.drawString(10*cm, 22.5*cm, u"Auto*Mat, o.s.")
-    canvas.setFont('DejaVu', 10)
-    canvas.drawString(10*cm, 22*cm, u"Lublaňská 398/18")
-    canvas.drawString(10*cm, 21.5*cm, u"120 00 Praha 2")
-    canvas.drawString(10*cm, 21*cm, u"IČ: 22670319/ DIČ: CZ22670319")
-    canvas.drawString(10*cm, 20.5*cm, u"č. účtu: 217 359 444 / 0300")
-    canvas.drawString(10*cm, 20*cm, u"VS: %s%03d" % (invoice.exposure_date.year, invoice.sequence_number))
+    for payment in invoice.payment_set.order_by("user_attendance__userprofile__user__last_name", "user_attendance__userprofile__user__first_name"):
+       fee = payment.user_attendance.team.subsidiary.city.cityincampaign_set.get(campaign=invoice.campaign).admission_fee
+       invoice_gen.add_item(Item(1, fee, description=u"Platba účasti v kampani za %s" % (payment.user_attendance,), tax=21))
 
-    canvas.drawString(3*cm, 18.5*cm, u"Fakturujeme Vám startovní poplatek za účastníky akce %s." % invoice.campaign.name)
-    invoice_count = invoice.payment_set.count()
-    canvas.drawString(3*cm, 18*cm, u"Počet soutěžících, za které fakturujeme poplatek: %s" % invoice_count)
-    p = Paragraph(u"Jména účastníků: %s" % (", ".join([u.user_attendance.__unicode__() for u in invoice.payment_set.order_by("user_attendance__userprofile__user__last_name", "user_attendance__userprofile__user__first_name").all()])), styles['Normal'])
-    p.wrapOn(canvas, 17*cm, 10*cm)
-    p.drawOn(canvas, 3 * cm, 7.5 * cm)
-
-    # START OF THE DOCUMENT
-    st = Image(stamp_file, 4.66*cm, 2.16*cm)
-    st.drawOn(canvas, 11*cm, 10*cm)
-
-    payment_base = sum([u.user_attendance.team.subsidiary.city.cityincampaign_set.get(campaign=invoice.campaign).admission_fee for u in invoice.payment_set.all()])
-
-    #TODO: get the fee from the database
-    locale.setlocale(locale.LC_ALL, "cs_CZ.utf8")
-    canvas.drawRightString(18*cm, 7*cm, u"CELKEM: %s Kč" % locale.format("%0.1f", payment_base * 1.21))
-    canvas.drawRightString(18*cm, 6.5*cm, u"Bez daně: %s Kč" % (locale.format("%0.1f", payment_base)))
-    canvas.drawRightString(18*cm, 6*cm, u"DPH 21,0%%: %s Kč" % locale.format("%0.1f", payment_base * 0.21))
-    canvas.drawRightString(18*cm, 5.5*cm, u"CELKEM K ÚHRADĚ vč. DPH: %s Kč" % (locale.format("%0.1f", payment_base)))
-    canvas.drawRightString(18*cm, 5*cm, u"Částka k úhradě: %s Kč" % (locale.format("%0.1f", payment_base)))
-
-    canvas.drawString(3*cm, 4*cm, u"V Praze, dne ")
-    canvas.drawString(3*cm, 3.5*cm, u"vystavil: Lucie Mullerová/737 563 750")
-    canvas.drawString(3*cm, 3*cm, u"lucie.mullerova@auto-mat.cz")
-
-    canvas.drawString(3*cm, 2.5*cm, u"Sdružení Auto*Mat bylo zaregistrováno u MV ČR pod číslem VS/1-1/68 776/07-R.")
-    canvas.drawString(3*cm, 2*cm, u"U Městského soudu v Praze jsme vedeni pod spisovou značkou L 18119.")
-    canvas.drawString(3*cm, 1.5*cm, u"Společně s vámi tvoříme město, ve kterém chceme žít.")
-    canvas.drawString(3*cm, 1*cm, u"www.auto-mat.cz")
+    pdf = SimpleInvoice(invoice_gen)
+    pdf.gen(outfile, generate_qr_code=True)
