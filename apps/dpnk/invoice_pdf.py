2d81f97f3726972b9d33ca3048325eb7b6c8509a
Petr Dlouhý <petr.dlouhy@email.cz>
2014-04-17 16:36:18 +0200

    improve invoice creation

--- apps/dpnk/invoice_pdf.py
+++ apps/dpnk/invoice_pdf.py
@@ -2,6 +2,7 @@
 import os
 from InvoiceGenerator.api import Invoice, Item, Client, Provider, Creator
 from InvoiceGenerator.pdf import SimpleInvoice
+import models
 
 
 def make_invoice_sheet_pdf(outfile, invoice):
@@ -26,6 +27,7 @@ def make_invoice_sheet_pdf(outfile, invoice):
         vat_id=u"CZ22670319",
         ir=u"22670319",
         phone=u"737 563 750",
+        logo_filename=os.path.join(DIR, "static/img/logo.jpg"),
         note=u"""Sdružení Auto*Mat bylo zaregistrováno u MV ČR pod č. VS/1-1/68 776/07-R.
 U Městského soudu v Praze jsme vedeni pod spisovou značkou L 18119.
 Společně s vámi tvoříme město, ve kterém chceme žít. www.auto-mat.cz
@@ -41,10 +43,13 @@ Společně s vámi tvoříme město, ve kterém chceme žít. www.auto-mat.cz
     invoice_gen.title = u"Faktura %s/%s" % (invoice.sequence_number, invoice.exposure_date.year)
     invoice_gen.variable_symbol = "%s%03d" % (invoice.exposure_date.year, invoice.sequence_number)
     invoice_gen.date = invoice.exposure_date
+    invoice_gen.currency = u"Kč"
 
-    for payment in invoice.payment_set.order_by("user_attendance__userprofile__user__last_name", "user_attendance__userprofile__user__first_name"):
-       fee = payment.user_attendance.team.subsidiary.city.cityincampaign_set.get(campaign=invoice.campaign).admission_fee
-       invoice_gen.add_item(Item(1, fee, description=u"Platba účasti v kampani za %s" % (payment.user_attendance,), tax=21))
+    for subsidiary in models.Subsidiary.objects.filter(teams__users__transactions__payment__invoice=invoice).distinct():
+       competitors = invoice.payment_set.filter(user_attendance__team__subsidiary=subsidiary).order_by("user_attendance__userprofile__user__last_name", "user_attendance__userprofile__user__first_name")
+       if competitors.exists():
+          fee = subsidiary.city.cityincampaign_set.get(campaign=invoice.campaign).admission_fee
+          invoice_gen.add_item(Item(competitors.count(), fee, description=u"Platba účasti v kampani za %s na pobočce %s, %s za soutěžící %s" % (invoice.campaign, subsidiary.address.street, subsidiary.address.city, ", ".join([u.user_attendance.__unicode__() for u in competitors])), tax=21))
 
     pdf = SimpleInvoice(invoice_gen)
     pdf.gen(outfile, generate_qr_code=True)
