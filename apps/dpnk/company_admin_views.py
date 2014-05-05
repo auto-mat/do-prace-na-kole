562b8af86985d95067ef8c1b5e74c4c8e1474fe3
Petr Dlouhý <petr.dlouhy@email.cz>
2014-05-05 11:51:57 +0200

    don't allow creating invoices for company coordinators without right to confirm payments

--- apps/dpnk/company_admin_views.py
+++ apps/dpnk/company_admin_views.py
@@ -233,6 +233,7 @@ def competitions(
 
 @must_be_company_admin
 @login_required
+@request_condition(lambda r, a, k: not k['company_admin'].can_confirm_payments, "<div class='text-warning'>" + ugettext(u"Vystavování faktur nemáte povoleno") + "</div>")
 def invoices(
         request,
         template='company_admin/invoices.html',
@@ -274,6 +275,7 @@ class CreateInvoiceView(FormView):
     @method_decorator(must_be_company_admin)
     @method_decorator(request_condition(lambda r, a, k: not k['company_admin'].administrated_company.has_filled_contact_information(), "<div class='text-warning'>" + ugettext(u"Před vystavením faktury prosím <a href='%s'>vyplňte údaje o vaší firmě</a>" % wp_reverse('edit_company')) + "</div>"))
     @method_decorator(request_condition(lambda r, a, k: k['company_admin'].company_has_invoices(), "<div class='text-warning'>" + ugettext(u"Vaše společnost již má fakturu vystavenou") + "</div>"))
+    @method_decorator(request_condition(lambda r, a, k: not k['company_admin'].can_confirm_payments, "<div class='text-warning'>" + ugettext(u"Vystavování faktur nemáte povoleno") + "</div>"))
     def dispatch(self, request, *args, **kwargs):
         self.company_admin = kwargs['company_admin']
         return super(CreateInvoiceView, self).dispatch(request, *args, **kwargs)
