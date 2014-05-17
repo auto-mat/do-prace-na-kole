b6ff880acc0a28bb726f91f39e79274b136006c6
Petr Dlouhý <petr.dlouhy@email.cz>
2014-05-17 13:13:49 +0200

    CompetitionAdmin: search by slug

--- apps/dpnk/admin.py
+++ apps/dpnk/admin.py
@@ -173,7 +173,7 @@ class QuestionInline(SortableInlineAdminMixin, EnhancedAdminMixin, admin.Tabular
 class CompetitionAdmin(EnhancedModelAdminMixin, ImportExportModelAdmin, RelatedFieldAdmin):
     list_display = ('name', 'slug', 'type', 'competitor_type', 'without_admission', 'is_public', 'date_from', 'date_to', 'entry_after_beginning_days', 'city', 'company__name', 'competition_results_link', 'questionnaire_results_link', 'questionnaire_link', 'draw_link', 'get_competitors_count', 'url', 'id')
     filter_horizontal = ('team_competitors', 'company_competitors', 'user_attendance_competitors',)
-    search_fields = ('name', 'company__name')
+    search_fields = ('name', 'company__name', 'slug')
     list_filter = ('campaign', 'city', 'without_admission', 'is_public', 'competitor_type', 'type')
     save_as = True
     actions = [recalculate_competitions_results]
