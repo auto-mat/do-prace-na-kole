ccbbb94f7cc0d07cba5ae635c7fc81bfbb90b680
Petr Dlouhý <petr.dlouhy@email.cz>
2014-05-19 16:40:45 +0200

    results cache back to one hour

--- apps/dpnk/views.py
+++ apps/dpnk/views.py
@@ -774,7 +774,7 @@ def admissions(
         }, context_instance=RequestContext(request))
 
 
-@cache_page(60)
+@cache_page(60*60)
 def competition_results(request, template, competition_slug, campaign_slug, limit=None):
     if limit == '':
         limit = None
