4a8f6364bc6a03c519c4301f51852f6bae8bd402
Petr Dlouhý <petr.dlouhy@email.cz>
2014-05-20 17:57:57 +0200

    add newrelic

--- wsgi.py
+++ wsgi.py
@@ -17,6 +17,9 @@ import os
 import site
 import sys
 from project.settings import PROJECT_ROOT
+import newrelic.agent
+
+newrelic.agent.initialize(os.path.join(PROJECT_ROOT,'newrelic.ini'))
 
 ALLDIRS = [ os.path.join(PROJECT_ROOT, 'env/lib/python2.6/site-packages'), ]
 
@@ -42,6 +45,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
