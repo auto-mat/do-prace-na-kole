082d1aa140e4db376e6d630b8624169c00c348fe
Petr Dlouhý <petr.dlouhy@email.cz>
2015-03-09 14:50:33 +0100

    password reset link position fix

--- apps/dpnk/forms.py
+++ apps/dpnk/forms.py
@@ -57,11 +57,11 @@ class AuthenticationFormDPNK(AuthenticationForm):
         self.helper = FormHelper()
         self.helper.form_class = "login-form"
         self.helper.layout = Layout(
+            'username', 'password',
             HTML(_(u"""<a href="%s">Zapomněli jste své přihlašovací údaje?</a>
                 <br/><br/>
                 Ještě nemáte účet? <a href="%s">Registrujte se</a> do soutěže Do práce na kole.<br/><br/>
             """ % ( reverse("password_reset"), reverse("registrace")) )),
-            'username', 'password',
             )
         self.helper.add_input(Submit('submit', _(u'Přihlásit')))
         self.fields['username'].label = _(u"Email (uživatelské jméno)")
