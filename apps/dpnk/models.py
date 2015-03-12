5e1d552fe645cbbe4dd9b1ad235a4a13db9fb86f
Petr Dlouhý <petr.dlouhy@email.cz>
2015-03-12 22:17:35 +0100

    fix NoneType error

--- apps/dpnk/models.py
+++ apps/dpnk/models.py
@@ -846,9 +846,10 @@ class UserAttendance(models.Model):
                if previous_user_attendance.track:
                    self.distance = previous_user_attendance.distance
                    self.track = previous_user_attendance.track
-               t_shirt_size = self.campaign.tshirtsize_set.filter(name=previous_user_attendance.t_shirt_size.name)
-               if t_shirt_size.count() == 1:
-                   self.t_shirt_size = t_shirt_size.first()
+               if previous_user_attendance.t_shirt_size:
+                   t_shirt_size = self.campaign.tshirtsize_set.filter(name=previous_user_attendance.t_shirt_size.name)
+                   if t_shirt_size.count() == 1:
+                       self.t_shirt_size = t_shirt_size.first()
         return super(UserAttendance, self).save(*args, **kwargs)
 
 
