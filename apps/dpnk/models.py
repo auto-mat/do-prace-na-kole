188817c723b32f7daa49b4dba13e8657c9374d81
Petr Dlouhý <petr.dlouhy@email.cz>
2015-03-31 23:59:59 +0200

    better creating trips in WorkingSchedule widget

--- apps/dpnk/models.py
+++ apps/dpnk/models.py
@@ -860,16 +860,23 @@ Trasa slouží k výpočtu vzdálenosti a pomůže nám lépe určit potřeby li
 
     def get_all_trips(self):
         days = util.days(self.campaign)
-        trips = []
-        for d in days:
+        trip_days = Trip.objects.filter(user_attendance=self, date__in=days).values_list('date', flat=True)
+        not_created_days = list(set(days) - set(trip_days))
+        create_trips = []
+        for d in not_created_days:
             working_day = util.working_day(d)
-            trip, created = self.user_trips.get_or_create(date=d, defaults={
-                'is_working_ride_to': working_day,
-                'is_working_ride_from': working_day
-                })
-            trip.can_edit = d >= util.today()
+            create_trips.append(Trip(date=d,
+                user_attendance=self,
+                is_working_ride_to=working_day,
+                is_working_ride_from=working_day,
+            ))
+        Trip.objects.bulk_create(create_trips)
+
+        trips = Trip.objects.filter(user_attendance=self, date__in=days).all()
+        for trip in trips:
+            working_day = util.working_day(trip.date)
+            trip.can_edit = trip.date >= util.today()
             trip.working_day = working_day
-            trips.append(trip)
         return trips
 
     def is_company_admin(self):
