ea62ca4fa78b2dbc2943018fc557a8581a634f79
Petr Dlouhý <petr.dlouhy@email.cz>
2015-05-26 12:56:58 +0200

    AnswerAdmin: add user email to list_fields

--- apps/dpnk/admin.py
+++ apps/dpnk/admin.py
@@ -730,7 +730,7 @@ class HasReactionFilter(SimpleListFilter):
 
 
 class AnswerAdmin(EnhancedModelAdminMixin, RelatedFieldAdmin):
-    list_display = ('user_attendance', 'points_given', 'choices_ids_all', 'question__competition', 'comment', 'choices_all', 'attachment_url', 'comment', 'question__text')
+    list_display = ('user_attendance', 'user_attendance__userprofile__user__email', 'points_given', 'choices_ids_all', 'question__competition', 'comment', 'choices_all', 'attachment_url', 'comment', 'question__text')
     search_fields = ('user_attendance__userprofile__nickname', 'user_attendance__userprofile__user__first_name', 'user_attendance__userprofile__user__last_name', 'question__text', 'question__name', 'question__competition__name', 'user_attendance__team__subsidiary__company__name')
     list_filter = (QuestionCampaignFilter, HasReactionFilter, 'question__competition__city', 'question__competition')
     filter_horizontal = ('choices',)
