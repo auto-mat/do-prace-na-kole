e8e6277a424276d2ee8fa5329eb2bc2879d4cec5
Petr Dlouhý <petr.dlouhy@email.cz>
2015-12-28 16:11:35 +0100

    fix GPXFile view, with tests

--- apps/dpnk/templatetags/dpnk_tags.py
+++ apps/dpnk/templatetags/dpnk_tags.py
@@ -23,6 +23,8 @@ from django.template import Context
 from django.template.loader import get_template
 from cache_utils.decorators import cached
 from django.utils.safestring import mark_safe
+from django.core.urlresolvers import resolve, reverse
+from django.utils.translation import activate, get_language
 import slumber
 register = template.Library()
 
@@ -82,3 +84,24 @@ def split(str, splitter):
 @register.filter
 def times(count):
     return range(int(count))
+
+
+@register.simple_tag(takes_context=True)
+def change_lang(context, lang=None, *args, **kwargs):
+    """
+    Get active page's url by a specified language
+    Usage: {% change_lang 'en' %}
+    """
+
+    path = context['request'].path
+    url_parts = resolve(path)
+
+    url = path
+    cur_language = get_language()
+    try:
+        activate(lang)
+        url = reverse(url_parts.view_name, kwargs=url_parts.kwargs)
+    finally:
+        activate(cur_language)
+
+    return "%s" % url
