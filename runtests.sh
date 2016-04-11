b996a0216729e644d9f5247726af50486ab5e517
Petr Dlouhý <petr.dlouhy@email.cz>
2016-04-11 09:22:53 +0200

    add --with-timer parameter to runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo /etc/init.d/memcached restart
-DJANGO_SETTINGS_MODULE="project.settings" env/bin/coverage run ./runtests.py
+DJANGO_SETTINGS_MODULE="project.settings" env/bin/coverage run ./runtests.py $@ --with-timer
 DJANGO_SETTINGS_MODULE="project.settings" env/bin/coverage html
