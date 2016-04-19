ee7fc43a97309583da5698f879e8f89a756665c8
Petr Dlouhý <petr.dlouhy@email.cz>
2016-04-19 12:02:58 +0200

    fix runtests.sh settings dir

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo /etc/init.d/memcached restart
-DJANGO_SETTINGS_MODULE="project.settings" env/bin/coverage run ./runtests.py $@ --with-timer
-DJANGO_SETTINGS_MODULE="project.settings" env/bin/coverage html
+DJANGO_SETTINGS_MODULE="project.test_settings" env/bin/coverage run ./runtests.py $@ --with-timer
+DJANGO_SETTINGS_MODULE="project.test_settings" env/bin/coverage html
