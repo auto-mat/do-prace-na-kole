ff4b63208afdf77a014efe8af326ee6a10bde946
Petr Dlouhý <petr.dlouhy@email.cz>
2016-08-29 15:01:22 +0200

    improve runtests

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 export DJANGO_SETTINGS_MODULE="project.test_settings"
-env/bin/coverage run ./runtests.py $@ --with-timer && env/bin/coverage html
+source env/bin/activate
+flake8
+env/bin/coverage run ./runtests.py $@ --with-timer
+env/bin/coverage html
