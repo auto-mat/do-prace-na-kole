7cfa34c649f3f860ea404556f812ab6ae64140d4
Petr Dlouhý <petr.dlouhy@email.cz>
2016-10-18 11:45:35 +0200

    update Docker configuration

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ run apt-get install -y libtiff5-dev libjpeg-dev zlib1g-dev libfreetype6-dev libl
 run apt-get install -y libxml2-dev libxslt-dev
 run apt-get install -y node-less yui-compressor
 run apt-get install -y libgeos-dev
+run apt-get install -y gunicorn
 
 run mkdir /home/aplikace -p
 WORKDIR "/home/aplikace"
@@ -21,4 +22,6 @@ run pip install -r test_requirements.txt
 copy . .
 
 EXPOSE 8000
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+RUN mkdir media logs
+VOLUME ["logs", "media"]
+ENTRYPOINT ["./docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python manage.py migrate                  # Apply database migrations
+python manage.py collectstatic --noinput  # Collect static files
+
+# Prepare log files and start outputting logs to stdout
+touch logs/gunicorn.log
+touch logs/access.log
+tail -n 0 -f logs/*.log &
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn wsgi:application \
+    --name dpnk \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --log-level=info \
+    --log-file=/home/aplikace/logs/gunicorn.log \
+    --access-logfile=/home/aplikace/logs/access.log \
+    "$@"
