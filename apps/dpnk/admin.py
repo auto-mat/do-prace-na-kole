e371b43760366b3b94781c1ad2d280a577fe4617
Petr Dlouhý <petr.dlouhy@email.cz>
2016-12-13 18:30:05 +0100

    DiscountCouponAdmin: add user_attendance_number to list_editable

--- apps/dpnk/admin.py
+++ apps/dpnk/admin.py
@@ -1444,9 +1444,9 @@ class DiscountCouponTypeAdmin(ImportMixin, ExportMixin, admin.ModelAdmin):
 
 
 class DiscountCouponAdmin(ImportMixin, ExportMixin, RelatedFieldAdmin):
-    list_display = ('name', 'coupon_type__prefix', 'token', 'coupon_type', 'discount', 'note', 'receiver', 'sent')
+    list_display = ('name', 'coupon_type__prefix', 'token', 'coupon_type', 'discount', 'user_attendance_number', 'note', 'receiver', 'sent')
     readonly_fields = ('token', 'created', 'updated', 'author', 'updated_by')
-    list_editable = ('note', 'receiver', 'discount', 'sent')
+    list_editable = ('note', 'receiver', 'discount', 'user_attendance_number', 'sent')
     list_filter = ('coupon_type__name', 'sent')
 
 
