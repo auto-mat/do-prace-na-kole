2b71ee1bac802accb373f111056330d7863b7dc1
Petr Dlouhý <petr.dlouhy@email.cz>
2017-01-19 16:24:07 +0100

    move ResultsTests to test_results

--- dpnk/test/test_results.py
+++ dpnk/test/test_results.py
@@ -20,9 +20,11 @@
 import datetime
 
 from django.test import TestCase
+from django.test.utils import override_settings
 
-from dpnk import models, results
+from dpnk import models, results, util
 from dpnk.test.util import print_response  # noqa
+from dpnk.test.util import ClearCacheMixin, DenormMixin
 
 from model_mommy import mommy
 
@@ -54,3 +56,60 @@ class RidesBaseTests(TestCase):
         self.assertEquals(results.get_minimum_rides_base_proportional(competition, datetime.date(2010, 11, 7)), 5)
         self.assertEquals(results.get_minimum_rides_base_proportional(competition, datetime.date(2010, 11, 15)), 12)
         self.assertEquals(results.get_minimum_rides_base_proportional(competition, datetime.date(2010, 11, 30)), 25)
+
+
+class ResultsTests(DenormMixin, ClearCacheMixin, TestCase):
+    fixtures = ['sites', 'campaign', 'auth_user', 'users', 'test_results_data', 'trips']
+
+    def setUp(self):
+        super().setUp()
+        util.rebuild_denorm_models(models.Team.objects.filter(pk=1))
+        util.rebuild_denorm_models(models.UserAttendance.objects.filter(pk=1115))
+
+    def test_get_competitors(self):
+        team = models.Team.objects.get(id=1)
+        query = results.get_competitors(models.Competition.objects.get(id=0))
+        self.assertListEqual(list(query.all()), [team])
+
+    def test_get_userprofile_length(self):
+        user_attendance = models.UserAttendance.objects.get(pk=1115)
+        competition = models.Competition.objects.get(id=5)
+        result = results.get_userprofile_length([user_attendance], competition)
+        self.assertEquals(result, 5.0)
+
+        result = user_attendance.trip_length_total
+        self.assertEquals(result, 5.0)
+
+    @override_settings(
+        FAKE_DATE=datetime.date(year=2010, month=11, day=20),
+    )
+    def test_get_userprofile_frequency(self):
+        user_attendance = models.UserAttendance.objects.get(pk=1115)
+        competition = models.Competition.objects.get(id=3)
+
+        result = user_attendance.get_rides_count_denorm
+        self.assertEquals(result, 1)
+
+        result = user_attendance.get_working_rides_base_count()
+        self.assertEquals(result, 31)
+
+        result = user_attendance.frequency
+        self.assertEquals(result, 0.0222222222222222)
+
+        result = user_attendance.team.frequency
+        self.assertEquals(result, 0.0075187969924812)
+
+        result = user_attendance.team.get_rides_count_denorm
+        self.assertEquals(result, 1)
+
+        result = user_attendance.team.get_working_trips_count()
+        self.assertEquals(result, 91)
+
+        result = results.get_working_trips_count(user_attendance, competition)
+        self.assertEquals(result, 23)
+
+        result = results.get_userprofile_frequency(user_attendance, competition)
+        self.assertEquals(result, (1, 23, 1 / 23.0))
+
+        result = results.get_team_frequency(user_attendance.team.members(), competition)
+        self.assertEquals(result, (1, 69, 1 / 69.0))
--- dpnk/test/tests.py
+++ dpnk/test/tests.py
@@ -29,7 +29,7 @@ from django.core.urlresolvers import reverse
 from django.test import Client, RequestFactory, TestCase
 from django.test.utils import override_settings
 
-from dpnk import actions, company_admin_views, models, results, util, views
+from dpnk import actions, company_admin_views, models, util, views
 from dpnk.test.util import ClearCacheMixin, DenormMixin
 from dpnk.test.util import print_response  # noqa
 
@@ -603,63 +603,6 @@ class TestTeams(DenormMixin, ClearCacheMixin, TestCase):
         denorm.flush()
         team = models.Team.objects.get(id=1)
         self.assertEqual(team.member_count, 4)
-
-
-class ResultsTests(DenormMixin, ClearCacheMixin, TestCase):
-    fixtures = ['sites', 'campaign', 'auth_user', 'users', 'test_results_data', 'trips']
-
-    def setUp(self):
-        super().setUp()
-        util.rebuild_denorm_models(models.Team.objects.filter(pk=1))
-        util.rebuild_denorm_models(models.UserAttendance.objects.filter(pk=1115))
-
-    def test_get_competitors(self):
-        team = models.Team.objects.get(id=1)
-        query = results.get_competitors(models.Competition.objects.get(id=0))
-        self.assertListEqual(list(query.all()), [team])
-
-    def test_get_userprofile_length(self):
-        user_attendance = models.UserAttendance.objects.get(pk=1115)
-        competition = models.Competition.objects.get(id=5)
-        result = results.get_userprofile_length([user_attendance], competition)
-        self.assertEquals(result, 5.0)
-
-        result = user_attendance.trip_length_total
-        self.assertEquals(result, 5.0)
-
-    @override_settings(
-        FAKE_DATE=datetime.date(year=2010, month=11, day=20),
-    )
-    def test_get_userprofile_frequency(self):
-        user_attendance = models.UserAttendance.objects.get(pk=1115)
-        competition = models.Competition.objects.get(id=3)
-
-        result = user_attendance.get_rides_count_denorm
-        self.assertEquals(result, 1)
-
-        result = user_attendance.get_working_rides_base_count()
-        self.assertEquals(result, 31)
-
-        result = user_attendance.frequency
-        self.assertEquals(result, 0.0222222222222222)
-
-        result = user_attendance.team.frequency
-        self.assertEquals(result, 0.0075187969924812)
-
-        result = user_attendance.team.get_rides_count_denorm
-        self.assertEquals(result, 1)
-
-        result = user_attendance.team.get_working_trips_count()
-        self.assertEquals(result, 91)
-
-        result = results.get_working_trips_count(user_attendance, competition)
-        self.assertEquals(result, 23)
-
-        result = results.get_userprofile_frequency(user_attendance, competition)
-        self.assertEquals(result, (1, 23, 1 / 23.0))
-
-        result = results.get_team_frequency(user_attendance.team.members(), competition)
-        self.assertEquals(result, (1, 69, 1 / 69.0))
 
 
 class ModelTests(DenormMixin, ClearCacheMixin, TestCase):
