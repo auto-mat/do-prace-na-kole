324837b34f0c78d5831b0bf6579a01b6a8ff1594
Petr Dlouhý <petr.dlouhy@email.cz>
2017-03-06 18:59:49 +0100

    Revert "fix flake8 linting"
    This reverts commit 0aee1aa9568c6fe10e8425e0873a4605a961fd50.

--- dpnk/results.py
+++ dpnk/results.py
@@ -20,7 +20,7 @@
 
 from django.db.models import Q, Sum
 
-from . import util
+from . import tasks, util
 from .models import Answer, Choice, City, Company, Competition, CompetitionResult, Team, Trip, UserAttendance
 
 
