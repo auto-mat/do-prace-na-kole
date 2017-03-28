00a1e68d79c2f0e8e4e3aa387b7eb0ed3ba73c23
Petr Dlouhý <petr.dlouhy@email.cz>
2017-03-28 17:39:32 +0200

    edit generate_delivery_batch script

--- scripts/generate_delivery_batch_pdf.sh
+++ scripts/generate_delivery_batch_pdf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pdftk $1 burst
+# Usage: ./generate_delivery_batch_pdf.sh in.pdf in.csv "ids"
+
+pdftk $1 burst output pdf_output/pg_%04d.pdf
 
 mkdir pdf_output
 
@@ -11,10 +13,10 @@ i=0
 for id in $file_ids
 do
    i=$((i+1))
-   out_id=`cat $2 | cut -d\; -f 1,2 | tr \; " " | sed 's/"//g' | grep "^$id " | cut -d " " -f 2`
-   mv pg_`printf "%04d" $i`.pdf pdf_output/customer_sheets_$out_id.a.pdf
+   out_id=`cat $2 | cut -d\; -f 1,8 | tr \; " " | sed 's/"//g' | grep "^$id " | cut -d " " -f 2`
+   mv `printf "pdf_output/pg_%04d" $i`.pdf pdf_output/customer_sheets_$out_id.a.pdf
 done
 
 
-pdftk pdf_output/* output combined_sheets.pdf
-pdftk combined_sheets.pdf  cat 1-endeast output combined_sheets-rotated.pdf
+pdftk pdf_output/*.pdf output combined_sheets.pdf
+pdftk combined_sheets.pdf cat 1-endeast output combined_sheets-rotated.pdf
