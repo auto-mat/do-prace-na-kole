0d8679f1e813b6342eccf3443b332884901bab35
Petr Dlouhý <petr.dlouhy@email.cz>
2017-04-18 16:36:36 +0200

    fix docker-celery-entrypoint permissions

--- docker-celery-entrypoint.sh
+++ docker-celery-entrypoint.sh
@@ -2,4 +2,4 @@
 
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
 echo Starting celery worker
-celery worker -A project.celery
+celery worker -A project.celery -l info
