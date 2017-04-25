a10382904f5786fe9ffaba79cd9995c45e0ffe4b
Petr Dlouhý <petr.dlouhy@email.cz>
2017-04-25 08:20:37 +0200

    fix celerybeat endpoint

--- docker-celerybeat-entrypoint.sh
+++ docker-celerybeat-entrypoint.sh
@@ -2,4 +2,4 @@
 
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
 echo Starting celery beat
-celery beat -A project.celery -S django
+celery beat -A project.celery -S django -l info
