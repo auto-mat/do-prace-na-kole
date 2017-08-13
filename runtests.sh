42083e383efa490f4efee3caab19436eccb9bb37
Petr Dlouhý <petr.dlouhy@email.cz>
2017-08-13 12:04:35 +0200

    fix: run AdminSmokeTests without making coverage from them

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 export DJANGO_SETTINGS_MODULE="project.test_settings"
-coverage run ./runtests.py $@ --with-timer -e dpnk.test.test_admin.AdminSmokeTests
+coverage run ./runtests.py $@ --with-timer -e dpnk.test.test_admin_smoke
 python ./runtests.py dpnk.test.test_admin_smoke --with-timer
 coverage html
