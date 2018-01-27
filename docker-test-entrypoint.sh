8ba96e850972407894c5a11a410f609b7d7ef874
Petr Dlouhý <petr.dlouhy@email.cz>
2018-01-27 10:13:50 +0100

    test in various Django versions

--- docker-test-entrypoint.sh
+++ docker-test-entrypoint.sh
@@ -4,6 +4,7 @@
 echo Starting tests
 pip install -r requirements-test.freeze.txt
 flake8
+pip install "$DJANGO_VERSION"
 ./runtests.sh $TEST_PARAMS
 mkdir reports -p
 pip freeze > reports/requirements.freeze.txt
