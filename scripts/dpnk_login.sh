8bbc8aeaece6e934c1e7fc9e7c71b5f2c08adb82
Petr Dlouhý <petr.dlouhy@email.cz>
2018-05-04 15:09:49 +0200

    update performance testing script

--- scripts/dpnk_login.sh
+++ scripts/dpnk_login.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 # Log in to DPNK and save it into session in cookies.txt
-http_code=`curl --silent  --write-out %{http_code} "https://dpnk.dopracenakole.cz/admin/" --output /dev/null -c cookies.txt -b cookies.txt`
+HOST_NAME=${HOST_NAME:-"dpnk.dopracenakole.cz"}
+PROTOCOL=${PROTOCOL:-"https"}
+PORT=${PORT:-"443"}
+BASE_ADDRESS="$PROTOCOL://$HOST_NAME"
+ADDRESS="$BASE_ADDRESS:$PORT"
+echo connecting to $ADDRESS
+http_code=`curl --silent  --write-out %{http_code} "$ADDRESS/aplikace/" --output /dev/null -c cookies.txt -b cookies.txt`
+if [ $http_code = 000 ]; then
+   echo "Site not reachable"
+   exit 3
+fi
 if [ $http_code -ne 200 ]; then
-   curl --silent "https://dpnk.dopracenakole.cz/admin/login/" --output /dev/null -c cookies.txt -b cookies.txt
+   curl --silent "$ADDRESS/login/" --output output.html -c cookies.txt -b cookies.txt
    read -p 'Enter username: ' username
    read -sp 'Enter password: ' password
-   csrftoken=`grep csrftoken cookies.txt | cut -f 7`
-   http_code=`curl --write-out %{http_code} --silent "https://dpnk.dopracenakole.cz/admin/login/" --referer "https://dpnk.dopracenakole.cz/admin/login/" --output /dev/null  -c cookies.txt -b cookies.txt -d "username=$username" -d "password=$password" -d "csrfmiddlewaretoken=$csrftoken"`
-   [ $http_code -ne 302 ] && echo -e "\nWrong authentication" && exit 1
+   echo -e "\n"
+   csrftoken=`grep "$HOST_NAME.*csrftoken" cookies.txt | cut -f 7`
+   http_code=`curl --write-out %{http_code} --silent "$ADDRESS/login/" --referer "$BASE_ADDRESS/login/" --output output.html  -c cookies.txt -b cookies.txt -d "username=$username" -d "password=$password" -d "csrfmiddlewaretoken=$csrftoken"`
+   [ $http_code -ne 302 ] && echo -e "\nWrong authentication, returned code $http_code" && exit 1
 fi
 echo "Successfully logged into DPNK"
