2627c1479800ac4ea7e51d9b870f4957e0cd4d9d
Petr Dlouhý <petr.dlouhy@email.cz>
2018-05-14 12:55:25 +0200

    fix download_dispatched script time range

--- scripts/download_dispatched_subsidiary_boxes.sh
+++ scripts/download_dispatched_subsidiary_boxes.sh
@@ -8,7 +8,7 @@ if [ -z ${sessionID} ]; then
    read -p 'Enter session ID: ' sessionID
    echo -e "\n"
 fi
-file=`curl 'http://online.gls-czech.com/ll_getReportClass.php?uid=10424&lng=cz&fileType=CSV&dateFrom=1.04.2018&dateTo=30.04.2018&senderID=-1&replist=0-30&gapid=-1' -H "Cookie: PHPSESSID=$sessionID; SRV_ID=01; cookie[userdata][lessersecurity]=1; cookie[userdata][username]=$username; cookie[userdata][password]=$password"`
+file=`curl "http://online.gls-czech.com/ll_getReportClass.php?uid=10424&lng=cz&fileType=CSV&dateFrom=$dateFrom&dateTo=$dateTo&senderID=-1&replist=0-30&gapid=-1" -H "Cookie: PHPSESSID=$sessionID; SRV_ID=01; cookie[userdata][lessersecurity]=1; cookie[userdata][username]=$username; cookie[userdata][password]=$password"`
 echo $file
 echo "carrier_identification,id,dispatched" > dispatched_subsidiary_boxes.csv
 curl "http://online.gls-czech.com/$file" -H "Cookie: PHPSESSID=$sessionID; SRV_ID=01; cookie[userdata][lessersecurity]=1; cookie[userdata][username]=$username; cookie[userdata][password]=$password" > gls_data.csv
