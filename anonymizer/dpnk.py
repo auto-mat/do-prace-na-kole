d39ea052f7d60db93ed0a8b08e29d72234fd41cb
Timothy Hobbs <timothy@hobbs.cz>
2019-03-10 21:13:12 +0100

    Fix flake8 errors

--- anonymizer/dpnk.py
+++ anonymizer/dpnk.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-
+from dj_anonymizer import anonym_field
 from dj_anonymizer.register_models import (
     AnonymBase,
-    register_anonym
+    register_anonym,
 )
-from dj_anonymizer import anonym_field
+
+from django.contrib.auth.models import User
 
 from faker import Factory
 
@@ -20,8 +19,10 @@ fake = Factory.create()
 class UserAnonym(AnonymBase):
     last_name = anonym_field.function(fake.last_name)
     first_name = anonym_field.function(fake.first_name)
-    email = anonym_field.string("timothy.hobbs+testuser{seq}@auto-mat.cz",
-                                seq_callback=datetime.now)
+    email = anonym_field.string(
+        "timothy.hobbs+testuser{seq}@auto-mat.cz",
+        seq_callback=datetime.now,
+    )
     username = anonym_field.string("user_name{seq}")
 
     class Meta:
@@ -31,7 +32,7 @@ class UserAnonym(AnonymBase):
         exclude_fields = [
             "groups", "user_permissions", "is_active",
             "is_superuser", "last_login", "date_joined",
-            "is_staff", "password"
+            "is_staff", "password",
         ]
 
 
