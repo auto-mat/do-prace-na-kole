76dc2ac8a041dfcc6c9e52076ea144782a321624
Timothy Hobbs <timothy@hobbs.cz>
2019-03-10 21:55:41 +0100

    Move to using syncsitetrees in setup.py

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ python3 manage.py collectstatic
 python3 manage.py migrate
 python3 manage.py createsuperuser
 python3 manage.py loaddata dpnk/fixtures/commute_mode.json
-python3 manage.py loaddata dpnk/fixtures/sitetree.json
 python3 manage.py loaddata dpnk/fixtures/sites.json
+python3 manage.py sitetree_resync_apps
 python3 manage.py import_czech_psc
 python3 manage.py loaddata dpnk/fixtures/occupation.json
 django-admin compilemessages
