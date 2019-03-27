74fc587096dbfcd1066fba321b8ace81a7ac6e2e
Petr Dlouhý <petr.dlouhy@email.cz>
2019-03-27 16:31:41 +0100

    relax on the unique Campaign type slug for now

--- dpnk/migrations/0128_auto_20190326_1245.py
+++ dpnk/migrations/0128_auto_20190326_1245.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='campaigntype',
             name='slug',
-            field=models.SlugField(blank=True, unique=True, verbose_name='Identifikátor typu kampaně'),
+            field=models.SlugField(blank=True, verbose_name='Identifikátor typu kampaně'),
         ),
         migrations.AlterField(
             model_name='payment',
