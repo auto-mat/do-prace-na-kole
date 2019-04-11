738fec35ef44bc81c0fc59578b389f496eb38fc1
Petr Dlouhý <petr.dlouhy@email.cz>
2019-04-11 12:04:46 +0200

    set dbbackups to S3

--- project/settings/base.py
+++ project/settings/base.py
@@ -657,3 +657,10 @@ def photologue_path(instance, filename):
 
 
 PHOTOLOGUE_PATH = photologue_path
+
+DBBACKUP_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DBBACKUP_STORAGE_OPTIONS = {
+    'access_key': os.environ.get('DPNK_AWS_ACCESS_KEY_ID'),
+    'secret_key': os.environ.get('DPNK_AWS_SECRET_ACCESS_KEY'),
+    'bucket_name': 'dpnk-dbbackups'
+}
