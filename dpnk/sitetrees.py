ba1f716871966e3b08f1d6591dadfe039938e341
Timothy Hobbs <timothy@hobbs.cz>
2019-04-29 14:33:11 +0200

    Remove table view from site trees

--- dpnk/sitetrees.py
+++ dpnk/sitetrees.py
@@ -4,10 +4,7 @@ sitetrees = (
     tree(
         'maintree', 'Hlavní menu', items=[
             item(
-                'Zapsat jízdu!', 'calendar', title_en='Take a Ride!', children=[
-                    item('Kalendář', 'calendar', title_en='Calender'),
-                    item('Tabulka', 'rides', title_en='Table'),
-                ],),
+                'Zapsat jízdu!', 'calendar', title_en='Take a Ride!'),
             item(
                 'Profil', 'edit_profile_detailed', title_en='Profile', children=[
                     item('Změnit osobní údaje', 'edit_profile_detailed', title_en='Change Personal Details'),
