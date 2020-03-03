bc4910752daa55a1a59770aa111c99cb94331a67
Timothy Hobbs <timothy@hobbs.cz>
2020-03-03 17:29:34 +0100

    Finish up basic strava tests

--- apps/stravasync/tests.py
+++ apps/stravasync/tests.py
@@ -1,5 +1,7 @@
+import datetime
 from unittest.mock import patch
 
+from django.test.utils import override_settings
 from django.urls import reverse
 
 from dpnk.test.test_views import ViewsLogon
@@ -21,6 +23,27 @@ class MockAthlete():
         self.lastname = 'Smith'
 
 
+class MockActivity():
+    def __init__(self, id, type, name=None):  # noqa
+        self.name = '#testing-campaigntam ' + str(id)
+        if name:
+            self.name = name
+        self.description = ""
+        self.start_date = lambda: None
+        self.start_date.date = lambda: datetime.date(2010, 11, id + 1)
+        self.map = lambda: None
+        self.map.summary_polyline = "_p~iF~ps|U_ulLnnqC_mqNvxq`@"
+        self.id = id
+        self.type = type
+        self.distance = 3
+        self.elapsed_time = lambda: None
+        self.elapsed_time.total_seconds = lambda: 34
+
+
+@override_settings(
+    FAKE_DATE=datetime.date(year=2010, month=11, day=5),
+    STRAVA_FINE_POLYLINES=False,
+)
 class TestStravaAuth(ViewsLogon):
 
     @patch('stravalib.Client')
@@ -91,7 +114,7 @@ class TestStravaAuth(ViewsLogon):
         self.assertEqual(strava_account.errors, "")
 
     @patch('stravalib.Client')
-    def test_strava_sync_no_activities(self, mock_strava_client):
+    def test_strava_sync_activities(self, mock_strava_client):
         from stravasync.tasks import sync
         msc = mock_strava_client()
         msc.exchange_code_for_token.return_value = mock_token_response
@@ -101,13 +124,28 @@ class TestStravaAuth(ViewsLogon):
             "refresh_token": "test_refresh_token",
         }
         msc.get_activities.return_value = [
+            MockActivity(1, "ride"),
+            MockActivity(2, "run"),
+            MockActivity(3, "non-existant-commute-mode"),
+            MockActivity(4, "walk", name="other"),
         ]
         self.client.get(reverse('strava_auth'))
         strava_account = StravaAccount.objects.all().first()
         stats = sync(strava_account.pk)
-        self.assertEqual(stats["activities"], [])
-        self.assertEqual(stats["new_trips"], 0)
-        self.assertEqual(stats["synced_trips"], 0)
-        self.assertEqual(stats["synced_activities"], 0)
         strava_account = StravaAccount.objects.all().first()
-        self.assertEqual(strava_account.errors, "")
+        self.assertEqual(
+            stats["activities"],
+            [
+                '#testing-campaigntam 1',
+                '#testing-campaigntam 2',
+                '#testing-campaigntam 3',
+                'other',
+            ],
+        )
+        self.assertEqual(stats["new_trips"], 2)
+        self.assertEqual(stats["synced_trips"], 3)
+        self.assertEqual(stats["synced_activities"], 4)
+        self.assertEqual(
+            strava_account.errors,
+            "Error syncing activity #testing-campaigntam 3 \nUnknown activity type 'non-existant-commute-mode'\n\n",
+        )
