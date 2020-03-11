5f4b0efcf73443960325b8e8debd2333307da84f
Timothy Hobbs <timothy@hobbs.cz>
2020-03-11 13:48:29 +0100

    Move to using ngnix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,9 +15,10 @@ service memcached restart
 echo Starting Gunicorn.
 exec gunicorn wsgi:application \
 	 --name dpnk \
-	 --bind 0.0.0.0:${GUNICORN_PORT:-"80"} \
+	 --bind 0.0.0.0:${GUNICORN_PORT:-"8000"} \
 	 --workers ${GUNICORN_NUM_WORKERS:-"6"} \
 	 --timeout ${GUNICORN_TIMEOUT:-"60"} \
+   --preload
 	 --log-level=debug \
 	 --log-file=- \
 	 --access-logfile=- \
