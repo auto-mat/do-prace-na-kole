839a5d7838819676a2733eb0f83a367809144b59
Timothy Hobbs <timothyhobbs@seznam.cz>
2020-03-31 19:23:02 +0200

    Flake8

--- apps/t_shirt_delivery/models/subsidiary_box.py
+++ apps/t_shirt_delivery/models/subsidiary_box.py
@@ -173,6 +173,7 @@ def create_customer_sheets(sender, instance, created, **kwargs):
     if not instance.customer_sheets and getattr(instance, 'add_packages_on_save', True):
         create_customer_sheets_action(queryset=[instance])
 
+
 def create_customer_sheets_action(modeladmin=None, request=None, queryset=None):
     for box in queryset:
         with NamedTemporaryFile() as temp:
@@ -180,5 +181,5 @@ def create_customer_sheets_action(modeladmin=None, request=None, queryset=None):
             box.customer_sheets.save("customer_sheets_%s_%s.pdf" % (box.pk, box.created.strftime("%Y-%m-%d")), File(temp))
             box.save()
 
-create_customer_sheets_action.short_description = _("Přegenerovat PDF")
 
+create_customer_sheets_action.short_description = _("Přegenerovat PDF")
