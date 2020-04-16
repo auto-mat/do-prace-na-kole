f064ab7903ba7205f3117b8963364e4e89b5c628
Petr Dlouhý <petr.dlouhy@email.cz>
2020-04-16 15:06:48 +0200

    fix page order in delivery batch combined pdf

--- scripts/batch_generation/generate_delivery_batch_pdf.sh
+++ scripts/batch_generation/generate_delivery_batch_pdf.sh
@@ -7,11 +7,11 @@ pdftk $1 burst output tmp_pdf/output/pg_%04d.pdf
 (
    cd tmp_pdf/output
    for file in pg_*.pdf; do
-      mv $file customer_sheets_`pdftotext $file - | grep "č. krab." | sed "s/č. krab. \([0-9]*\).*/\\1/"`__gls_label.pdf
+      mv $file customer_sheets_`pdftotext $file - | grep "č. krab." | sed "s/č. krab. \([0-9]*\).*/\\1/"`--gls_label.pdf
    done
 )
 
 echo concatenate pdf
-pdftk `ls tmp_pdf/output/*.pdf* | sort` cat output tmp_pdf/combined_sheets.pdf
+pdftk `LC_COLLATE=C ls tmp_pdf/output/*.pdf*` cat output tmp_pdf/combined_sheets.pdf
 echo rotate
 pdftk A=tmp_pdf/combined_sheets.pdf rotate Awest output tmp_pdf/combined_sheets-rotated.pdf
