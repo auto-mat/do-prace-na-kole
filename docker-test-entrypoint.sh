20970ebb02b2319069fd0ba3991a9e333f5a04ba
Timothy Hobbs <timothy@hobbs.cz>
2021-03-07 12:22:48 +0100

    Add comments to test entrypoint

--- docker-test-entrypoint.sh
+++ docker-test-entrypoint.sh
@@ -2,11 +2,14 @@
 
 # Start Gunicorn processes
 echo Starting tests
+echo Installing dev dependencies
 poetry install
+echo Running black check
 black --check --exclude migrations apps
 black --check project
-flake8
+echo Running tests
 ./runtests.sh $TEST_PARAMS
+echo Generating reports
 mkdir reports -p
 cp htmlcov/ reports -R
 cp .coverage reports/
