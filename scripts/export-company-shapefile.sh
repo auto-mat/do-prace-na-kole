0a09e0604f934a388f288d5681024b71f14cea45
Timothy Hobbs <timothy@hobbs.cz>
2021-11-05 12:26:36 +0100

    Upgrade company tracks export script

--- scripts/export-company-shapefile.sh
+++ scripts/export-company-shapefile.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-pgsql2shp -p 25061 -f $COMPANY_NAME.shp -h $DB_HOST -u dpnk -P $DB_PASS dpnk-pool 'select track from dpnk_trip where user_attendance_id in (select id from dpnk_userattendance where team_id in (select id from dpnk_team where subsidiary_id in (select id from dpnk_subsidiary where company_id = $COMPANY_ID))) and track is not null'
+pgsql2shp -p 25061 -f $COMPANY_NAME.shp -h $DB_HOST -u dpnk -P $DB_PASS dpnk-pool 'select track, date, direction, id from dpnk_trip where user_attendance_id in (select id from dpnk_userattendance where team_id in (select id from dpnk_team where subsidiary_id in (select id from dpnk_subsidiary where company_id = $COMPANY_ID))) and track is not null'
