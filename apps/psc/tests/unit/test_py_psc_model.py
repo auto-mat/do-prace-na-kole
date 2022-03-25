943a1ad0e7e0199a695eac51d54732294757cda4
Pavel Filippi <pavel.filippi@gmail.com>
2022-03-25 20:55:47 +0100

    Parametrize PSC unit tests
    Run tests in both CS/EN locale.
    EN translations missing - tests fail.

--- apps/psc/tests/unit/test_py_psc_model.py
+++ apps/psc/tests/unit/test_py_psc_model.py
@@ -17,55 +17,136 @@ def switch_language_fixture(request):
     deactivate()
 
 
-def test_model_verbose_name():
-    verbose_name = PSC._meta.verbose_name
+@pytest.mark.parametrize(
+    "switch_language, expected_verbose_name",
+    [
+        pytest.param("cs", "Poštovní směrovací číslo"),
+        pytest.param("en", "ZIP code"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_model_verbose_name(expected_verbose_name):
+    actual_verbose_name = PSC._meta.verbose_name
 
-    assert verbose_name == "Poštovní směrovací číslo"
-
-
-def test_model_verbose_name_plural():
-    verbose_name = PSC._meta.verbose_name_plural
-
-    assert verbose_name == "Poštovní směrovací čísla"
-
-
-def test_municipality_name_field_verbose_name():
-    verbose_name = PSC._meta.get_field("municipality_name").verbose_name
-
-    assert verbose_name == "Název obce"
+    assert actual_verbose_name == expected_verbose_name
 
 
-def test_municipality_part_name_field_verbose_name():
-    verbose_name = PSC._meta.get_field("municipality_part_name").verbose_name
+@pytest.mark.parametrize(
+    "switch_language, expected_verbose_name",
+    [
+        pytest.param("cs", "Poštovní směrovací čísla"),
+        pytest.param("en", "ZIP codes"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_model_verbose_name_plural(expected_verbose_name):
+    actual_verbose_name = PSC._meta.verbose_name_plural
 
-    assert verbose_name == "Název části obce"
-
-
-def test_district_name_field_verbose_name():
-    verbose_name = PSC._meta.get_field("district_name").verbose_name
-
-    assert verbose_name == "Název okresu"
-
-
-def test_psc_field_verbose_name():
-    verbose_name = PSC._meta.get_field("psc").verbose_name
-
-    assert verbose_name == "PSČ"
+    assert actual_verbose_name == expected_verbose_name
 
 
-def test_psc_field_help_text():
-    verbose_name = PSC._meta.get_field("psc").help_text
+@pytest.mark.parametrize(
+    "switch_language, expected_verbose_name",
+    [
+        pytest.param("cs", "Název obce"),
+        pytest.param("en", "Municipality name"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_municipality_name_field_verbose_name(expected_verbose_name):
+    actual_verbose_name = PSC._meta.get_field("municipality_name").verbose_name
 
-    assert verbose_name == "Např.: „130 00“"
+    assert actual_verbose_name == expected_verbose_name
 
 
-def test_post_name_field_verbose_name():
-    verbose_name = PSC._meta.get_field("post_name").verbose_name
+@pytest.mark.parametrize(
+    "switch_language, expected_verbose_name",
+    [
+        pytest.param("cs", "Název části obce"),
+        pytest.param("en", "Municipality part name"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_municipality_part_name_field_verbose_name(expected_verbose_name):
+    actual_verbose_name = PSC._meta.get_field("municipality_part_name").verbose_name
 
-    assert verbose_name == "Název pošty"
+    assert actual_verbose_name == expected_verbose_name
 
 
-def test_code_field_verbose_name():
-    verbose_name = PSC._meta.get_field("code").verbose_name
+@pytest.mark.parametrize(
+    "switch_language, expected_verbose_name",
+    [
+        pytest.param("cs", "Název okresu"),
+        pytest.param("en", "District name"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_district_name_field_verbose_name(expected_verbose_name):
+    actual_verbose_name = PSC._meta.get_field("district_name").verbose_name
 
-    assert verbose_name == "Kód"
+    assert actual_verbose_name == expected_verbose_name
+
+
+@pytest.mark.parametrize(
+    "switch_language, expected_verbose_name",
+    [
+        pytest.param("cs", "PSČ"),
+        pytest.param("en", "ZIP code"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_psc_field_verbose_name(expected_verbose_name):
+    actual_verbose_name = PSC._meta.get_field("psc").verbose_name
+
+    assert actual_verbose_name == expected_verbose_name
+
+
+@pytest.mark.parametrize(
+    "switch_language, expected_help_text",
+    [
+        pytest.param("cs", "Např.: „130 00“"),
+        pytest.param("en", "e.g. „130 00“"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_psc_field_help_text(expected_help_text):
+    actual_help_text = PSC._meta.get_field("psc").help_text
+
+    assert actual_help_text == expected_help_text
+
+
+@pytest.mark.parametrize(
+    "switch_language, expected_verbose_name",
+    [
+        pytest.param("cs", "Název pošty"),
+        pytest.param("en", "Post office name"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_post_name_field_verbose_name(expected_verbose_name):
+    actual_verbose_name = PSC._meta.get_field("post_name").verbose_name
+
+    assert actual_verbose_name == expected_verbose_name
+
+
+@pytest.mark.parametrize(
+    "switch_language, expected_verbose_name",
+    [
+        pytest.param("cs", "Kód"),
+        pytest.param("en", "Code"),
+    ],
+    indirect=["switch_language"],
+)
+@pytest.mark.usefixtures("switch_language")
+def test_code_field_verbose_name(expected_verbose_name):
+    actual_verbose_name = PSC._meta.get_field("code").verbose_name
+
+    assert actual_verbose_name == expected_verbose_name
