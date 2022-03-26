c601a0dacab719d020e4c50a37be55be568d8eec
Pavel Filippi <pavel.filippi@gmail.com>
2022-03-26 20:59:05 +0100

    Swap CS/EN in PSC model + locale files
    The idea here is to slowly migrate
    whole codebase to EN only - PR #231

--- apps/psc/models/psc.py
+++ apps/psc/models/psc.py
@@ -28,43 +28,43 @@ from django.utils.translation import ugettext_lazy as _, pgettext_lazy
 @with_author
 class PSC(models.Model):
     class Meta:
-        verbose_name = _("Poštovní směrovací číslo")
-        verbose_name_plural = _("Poštovní směrovací čísla")
+        verbose_name = _("ZIP code")
+        verbose_name_plural = _("ZIP codes")
 
     municipality_name = models.CharField(
-        verbose_name=_("Název obce"),
+        verbose_name=_("Municipality name"),
         max_length=255,
         null=True,
         blank=True,
     )
     municipality_part_name = models.CharField(
-        verbose_name=_("Název části obce"),
+        verbose_name=_("Municipality part name"),
         max_length=255,
         null=True,
         blank=True,
     )
     district_name = models.CharField(
-        verbose_name=_("Název okresu"),
+        verbose_name=_("District name"),
         max_length=255,
         null=True,
         blank=True,
     )
     psc = models.IntegerField(
-        verbose_name=pgettext_lazy("zip code model ctxt", "PSČ"),
-        help_text=_("Např.: „130 00“"),
+        verbose_name=pgettext_lazy("zip code model ctxt", "ZIP code"),
+        help_text=_("e.g. „130 00“"),
         validators=[
             MaxValueValidator(99999),
             MinValueValidator(10000),
         ],
     )
     post_name = models.CharField(
-        verbose_name=_("Název pošty"),
+        verbose_name=_("Post office name"),
         max_length=255,
         null=True,
         blank=True,
     )
     code = models.IntegerField(
-        verbose_name=_("Kód"),
+        verbose_name=_("Code"),
         null=True,
         blank=True,
     )
