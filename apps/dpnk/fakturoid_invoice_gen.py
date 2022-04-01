1ee42bfd2de40751b19fda3df8b9fd37b4022fd2
Tomas Zigo <tomas.zigo@slovanet.sk>
2022-04-01 08:10:25 +0200

    apps/dpnk: handle Fakturoid REST API exceptions

--- apps/dpnk/fakturoid_invoice_gen.py
+++ apps/dpnk/fakturoid_invoice_gen.py
@@ -1,11 +1,16 @@
 """Create Fakturoid invoice"""
 
 import decimal
+import logging
 import requests
 
 from django.conf import settings
 
 from fakturoid import Fakturoid, Invoice, InvoiceLine, Subject
+
+from .fakturoid_invoice_send_by_email import send_invoice_by_email
+
+logger = logging.getLogger(__name__)
 
 
 def create_or_update_subject(fa, invoice):
@@ -13,7 +18,17 @@ def create_or_update_subject(fa, invoice):
 
     :param class instance fa: fakturoid.Fakturoid class instance
     :param class instance: Invoice model class instance
+
+    :return Fakturoid Subject class instance/None fa_subject: Fakturoid
+                                                              Subject class
+                                                              instance
+                                                              or None
     """
+    error_message = (
+        "{state} Fakturoid subject {subject_custom_id}"
+        " wasn't succesfull with Fakturoid account"
+        " '{fakturoid_user_account_slug}' due error: '{error}'"
+    )
     # Subject
     fa_subject_data = {
         "custom_id": invoice.company.id,
@@ -38,10 +53,33 @@ def create_or_update_subject(fa, invoice):
     fa_subject = fa.subjects(custom_id=invoice.company.id)
     if not fa_subject:
         fa_subject = Subject(**fa_subject_data)
+        state = "Create"
+        subject_custom_id = ""
     else:
         fa_subject = fa_subject[0]
         fa_subject.update(fa_subject_data)
-    fa.save(fa_subject)
+        state = "Update"
+        subject_custom_id = f"with custom_id = {fa_subject.custom_id}"
+    try:
+        fa.save(fa_subject)
+    except (
+        requests.RequestException,
+        requests.ConnectionError,
+        requests.HTTPError,
+        requests.URLRequired,
+        requests.TooManyRedirects,
+        requests.Timeout,
+    ) as error:
+        logger.error(
+            error_message.format(
+                state=state,
+                subject_custom_id=subject_custom_id,
+                invoice_custom_id=invoice.id,
+                fakturoid_user_account_slug=fa.slug,
+                error=error,
+            )
+        )
+        fa_subject = None
     return fa_subject
 
 
@@ -85,8 +123,16 @@ def create_or_update_invoice(fa, subject, invoice):
     :param class instance subject: fakturoid.Subject model class instance
     :param class instance: Invoice model class instance
 
-    :return Fakturoid Invoice class instance: Fakturoid invoice
+    :return Fakturoid Invoice class instance/None fa_invoice: Fakturoid
+                                                              Invoice
+                                                              class instance
+                                                              or None
     """
+    error_message = (
+        "{state} Fakturoid invoice {subject_custom_id}"
+        " wasn't succesfull with Fakturoid account"
+        " '{fakturoid_user_account_slug}' due error: '{error}'"
+    )
     # Invoice
     fa_invoice = fa.invoices(custom_id=invoice.id)
     try:
@@ -106,75 +152,17 @@ def create_or_update_invoice(fa, subject, invoice):
     if not fa_invoice:
         fa_invoice_data["lines"] = create_invoice_lines(invoice=invoice)
         fa_invoice = Invoice(**fa_invoice_data)
+        state = "Create"
+        subject_custom_id = ""
     else:
         fa_invoice.lines = []
         fa.save(fa_invoice)
         fa_invoice_data["lines"] = create_invoice_lines(invoice=invoice)
         fa_invoice.update(fa_invoice_data)
-    fa.save(fa_invoice)
-    return fa_invoice
-
-
-def get_fakturoid_api(account):
-    """Get Fakturoid API class instance
-
-    :param str account: Fakturoid account type "production" or "test"
-
-    :return class instance fakturoid.Fakturoid: Fakturoid API class
-                                                instance
-    """
-    return Fakturoid(
-        slug=settings.FAKTUROID[account]["user_account"],
-        email=settings.FAKTUROID[account]["user_email"],
-        api_key=settings.FAKTUROID[account]["api_key"],
-        user_agent=settings.FAKTUROID[account]["user_agent_header"],
-    )
-
-
-def send_invoice_by_email(invoice, fa):
-    """Send invoce by email
-
-    :param class instance fa: fakturoid.Fakturoid class instance
-    :param class instance: Invoice model class instance
-    """
-    import logging
-
-    logger = logging.getLogger(__name__)
-    email_base_url = (
-        "https://app.fakturoid.cz/api/v2/"
-        "accounts/{fakturoid_user_account_slug}/invoices/{invoice_custom_id}/"
-        "message.json"
-    )
-    error_message = (
-        "Sending Fakturoid invoice with custom_id={invoice_custom_id}"
-        " by email wasn't succesfull with Fakturoid testing account"
-        " '{fakturoid_user_account_slug}' due error: '{error}'"
-    )
-
+        state = "Update"
+        subject_custom_id = f"with custom_id = {fa_invoice.custom_id}"
     try:
-        response = requests.post(
-            email_base_url.format(
-                fakturoid_user_account_slug=fa.slug,
-                invoice_custom_id=invoice.id,
-            ),
-            headers={
-                "User-Agent": fa.user_agent,
-                "Content-Type": "application/json",
-            },
-            auth=requests.auth.HTTPBasicAuth(
-                fa.email,
-                fa.api_key,
-            ),
-            data=f"{{'email': '{fa.email}'}}",
-        )
-        if not response.ok:
-            logger.error(
-                error_message.format(
-                    invoice_custom_id=invoice.id,
-                    fakturoid_user_account_slug=fa.slug,
-                    error=response.status_code,
-                )
-            )
+        fa.save(fa_invoice)
     except (
         requests.RequestException,
         requests.ConnectionError,
@@ -185,11 +173,51 @@ def send_invoice_by_email(invoice, fa):
     ) as error:
         logger.error(
             error_message.format(
-                invoice_custom_id=invoice.id,
+                state=state,
+                subject_custom_id=subject_custom_id,
                 fakturoid_user_account_slug=fa.slug,
                 error=error,
             )
         )
+        fa_invoice = None
+    return fa_invoice
+
+
+def get_fakturoid_api(account):
+    """Get Fakturoid API class instance
+
+    :param str account: Fakturoid account type "production" or "test"
+
+    :return class instance fakturoid.Fakturoid/None fa: Fakturoid API class
+                                                        instance or None
+    """
+    error_message = (
+        "Get Fakturoid API instance wasn't succesfull with Fakturoid"
+        " account '{fakturoid_user_account_slug}' due error: '{error}'"
+    )
+    try:
+        fa = Fakturoid(
+            slug=settings.FAKTUROID[account]["user_account"],
+            email=settings.FAKTUROID[account]["user_email"],
+            api_key=settings.FAKTUROID[account]["api_key"],
+            user_agent=settings.FAKTUROID[account]["user_agent_header"],
+        )
+    except (
+        requests.RequestException,
+        requests.ConnectionError,
+        requests.HTTPError,
+        requests.URLRequired,
+        requests.TooManyRedirects,
+        requests.Timeout,
+    ) as error:
+        logger.error(
+            error_message.format(
+                fakturoid_user_account_slug=fa.slug,
+                error=error,
+            )
+        )
+        fa = None
+    return fa
 
 
 def generate_invoice(invoice, fakturoid_account=None):
@@ -198,8 +226,11 @@ def generate_invoice(invoice, fakturoid_account=None):
     :param class instance: Invoice model class instance
     :param str account: Fakturoid account type "production" or "test"
 
-    :return Fakturoid Invoice class instance: Fakturoid invoice
+    :return Fakturoid Invoice class instance/None fa_invoice: Fakturoid
+                                                              Invoice or
+                                                              None
     """
+    fa_invoice = None
     manually_send_invoice = False
     date_from_create_invoices = settings.FAKTUROID["date_from_create_invoices"]
     if not fakturoid_account:
@@ -212,15 +243,17 @@ def generate_invoice(invoice, fakturoid_account=None):
             fakturoid_account = "production"
     if settings.FAKTUROID[fakturoid_account]["user_account"]:
         fa = get_fakturoid_api(account=fakturoid_account)
-        # Subject
-        fa_subject = create_or_update_subject(fa=fa, invoice=invoice)
-        # Invoice
-        fa_invoice = create_or_update_invoice(
-            fa=fa,
-            invoice=invoice,
-            subject=fa_subject,
-        )
-        # Send email
-        if manually_send_invoice:
-            send_invoice_by_email(invoice=invoice, fa=fa)
+        if fa:
+            # Subject
+            fa_subject = create_or_update_subject(fa=fa, invoice=invoice)
+            if fa_subject:
+                # Invoice
+                fa_invoice = create_or_update_invoice(
+                    fa=fa,
+                    invoice=invoice,
+                    subject=fa_subject,
+                )
+                # Send email
+                if manually_send_invoice:
+                    send_invoice_by_email(invoice=fa_invoice, fa=fa)
         return fa_invoice
