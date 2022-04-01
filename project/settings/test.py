a7c6c84b289808db19860b7edb68e9e82a79bd2a
Tomas Zigo <tomas.zigo@slovanet.sk>
2022-04-01 11:13:35 +0200

    project/settings: fix test DATABASE settings DPNK_DB_NAME global var name

--- project/settings/test.py
+++ project/settings/test.py
@@ -36,13 +36,13 @@ CACHES = {
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": os.environ.get("DB", "circle_test"),
+        "NAME": os.environ.get("DPNK_DB_NAME", "circle_test"),
         "USER": os.environ.get("DPNK_DB_USER", "ubuntu"),
         "PASSWORD": os.environ.get("DPNK_DB_PASSWORD", ""),
         "HOST": os.environ.get("DPNK_DB_HOST", "localhost"),
         "PORT": os.environ.get("DPNK_DB_PORT", ""),
         "TEST": {
-            "NAME": os.environ.get("DB", "circle_test"),
+            "NAME": os.environ.get("DPNK_DB_NAME", "circle_test"),
         },
     },
 }
