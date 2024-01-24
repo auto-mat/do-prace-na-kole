ad25f5275ffb171ba91d56c7477a99c6028f2900
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-01-24 19:30:25 +0100

    Allow connection to AWS Aurora DPNK RDS proxy via SSH tunnel

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+AUTOSSH="autossh"
+
 if poetry run python manage.py showmigrations | grep '\[ \]\|^[a-z]' | grep '[  ]' -B 1; then
    poetry run python manage.py migrate --noinput                # Apply database migrations
    poetry run python manage.py update_translation_fields
@@ -10,6 +13,18 @@ fi
 tail -n 0 -f logs/*.log &
 
 service memcached restart
+
+# AWS Aurora PG RDS proxy SSH tunnel
+if command -v "${AUTOSSH}" &> /dev/null && test -n "${AWS_AURORA_PG_RDS_DPNK_PROXY_REMOTE_HOST}";
+then
+    "${AUTOSSH}" -o ServerAliveInterval=20 \
+                 -o StrictHostKeyChecking=accept-new \
+                 -i /root/.ssh/aurora-rds-pg-dpnk-db-proxy.pem \
+                 -M 0 \
+                 -N \
+                 -L "${AWS_AURORA_PG_RDS_DPNK_PROXY_LOCALHOST_PORT-5432}":"${AWS_AURORA_PG_RDS_DPNK_PROXY_REMOTE_HOST}":"${AWS_AURORA_PG_RDS_DPNK_PROXY_REMOTE_HOST_PORT-5432}" \
+                 "${AWS_E2C_AURORA_PG_RDS_DPNK_PROXY_REMOTE_USERNAME_WITH_HOST}" &
+fi
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
