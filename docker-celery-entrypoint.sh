0325749bc98b8d94fc53ee552640401d2d32b4f8
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-01-29 10:29:11 +0100

    Connect to AWS Aurora PG RDS DPNK DB via central K8 aurora-rds-pg-dpnk-db-proxy host tunnel

--- docker-celery-entrypoint.sh
+++ docker-celery-entrypoint.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-AUTOSSH="autossh"
-
-# AWS Aurora PG RDS proxy SSH tunnel
-if command -v "${AUTOSSH}" &> /dev/null && test -n "${AWS_AURORA_PG_RDS_DPNK_PROXY_REMOTE_HOST}";
-then
-    "${AUTOSSH}" -o ServerAliveInterval=20 \
-                 -o StrictHostKeyChecking=accept-new \
-                 -i /root/.ssh/aurora-rds-pg-dpnk-db-proxy.pem \
-                 -M 0 \
-                 -N \
-                 -L "${AWS_AURORA_PG_RDS_DPNK_PROXY_LOCALHOST_PORT-5432}":"${AWS_AURORA_PG_RDS_DPNK_PROXY_REMOTE_HOST}":"${AWS_AURORA_PG_RDS_DPNK_PROXY_REMOTE_HOST_PORT-5432}" \
-                 "${AWS_E2C_AURORA_PG_RDS_DPNK_PROXY_REMOTE_USERNAME_WITH_HOST}" &
-fi
-
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
 echo "Starting celery worker"
 poetry run celery worker -A project.celery --pool=prefork --concurrency="${CELERY_CONCURENCY-4}" --loglevel="${CELERY_LOGLEVEL-info}"
