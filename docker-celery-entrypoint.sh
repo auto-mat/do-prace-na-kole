95731487368ca0d87dbddcc692153988427e551f
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-03-05 20:48:31 +0100

    Fix setting Celery Beat pidfile

--- docker-celery-entrypoint.sh
+++ docker-celery-entrypoint.sh
@@ -2,4 +2,4 @@
 
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
 echo "Starting celery worker"
-exec poetry run celery worker -A project.celery --pool=prefork --concurrency="${CELERY_CONCURENCY-4}" --loglevel="${CELERY_LOGLEVEL-info}" --pidfile=/tmp/celery-beat.pid
+exec poetry run celery worker -A project.celery --pool=prefork --concurrency="${CELERY_CONCURENCY-4}" --loglevel="${CELERY_LOGLEVEL-info}"
--- docker-celerybeat-entrypoint.sh
+++ docker-celerybeat-entrypoint.sh
@@ -2,4 +2,4 @@
 
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
 echo "Starting celery beat"
-exec poetry run celery beat -A project.celery --scheduler django_celery_beat.schedulers:DatabaseScheduler --loglevel="${CELERY_BEAT_LOGLEVEL-info}"
+exec poetry run celery beat -A project.celery --scheduler django_celery_beat.schedulers:DatabaseScheduler --loglevel="${CELERY_BEAT_LOGLEVEL-info}" --pidfile=/tmp/celery-beat.pid
