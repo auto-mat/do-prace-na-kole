9c8a626b31b36a07911d0a591c1301af983b5625
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-03-05 20:13:59 +0100

    Set Celery Beat pidfile

--- docker-celery-entrypoint.sh
+++ docker-celery-entrypoint.sh
@@ -2,4 +2,4 @@
 
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
 echo "Starting celery worker"
-poetry run celery worker -A project.celery --pool=prefork --concurrency="${CELERY_CONCURENCY-4}" --loglevel="${CELERY_LOGLEVEL-info}"
+exec poetry run celery worker -A project.celery --pool=prefork --concurrency="${CELERY_CONCURENCY-4}" --loglevel="${CELERY_LOGLEVEL-info}" --pidfile=/tmp/celery-beat.pid
