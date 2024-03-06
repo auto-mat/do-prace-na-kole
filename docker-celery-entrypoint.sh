2fa8bb12433f6f2f2838f9e9f8ec95f43372099f
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-03-06 12:30:58 +0100

    Remove unnecessary Docker Celery/Celery Beat entrypoint files

--- docker-celery-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# run Celery worker for our project myproject with Celery configuration stored in Celeryconf
-echo "Starting celery worker"
-exec poetry run celery worker -A project.celery --pool=prefork --concurrency="${CELERY_CONCURENCY-4}" --loglevel="${CELERY_LOGLEVEL-info}"
