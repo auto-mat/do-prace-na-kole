c93cd0e4fe1d956f5f127870f80597e448c4abc5
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-03-12 10:12:42 +0100

    scripts/batch_generation: fix vars names

--- scripts/batch_generation/download_dispatched_subsidiary_boxes.sh
+++ scripts/batch_generation/download_dispatched_subsidiary_boxes.sh
@@ -5,14 +5,14 @@ curl -X POST -H "Content-Type: application/x-www-form-urlencoded" \
      -c cookies.txt \
      "$GLS_BASE_URL/login.php"
 
-dateFrom=$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_FROM
+date_from=$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_FROM
 if [ "$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO" == "date +%d.%m.%Y" ]; then
-    dateTo=$($GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO)
+    date_to=$($GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO)
 else
-    dateTo=${GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO-$(date +%d.%m.%Y)}
+    date_to=${GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO-$(date +%d.%m.%Y)}
 fi
 
-file=$(curl -b cookies.txt "$GLS_BASE_URL/ll_getReportClass.php?uid=10424&lng=cz&fileType=CSV&dateFrom=$dateFrom&dateTo=$dateTo&senderID=-1&replist=0-30&gapid=-1")
+file=$(curl -b cookies.txt "$GLS_BASE_URL/ll_getReportClass.php?uid=10424&lng=cz&fileType=CSV&dateFrom=$date_from&dateTo=$date_to&senderID=-1&replist=0-30&gapid=-1")
 echo "carrier_identification,id,dispatched" > dispatched_subsidiary_boxes.csv
 echo "$GLS_BASE_URL/$file"
 curl -b cookies.txt -L "$GLS_BASE_URL/$file" > gls_data.csv
