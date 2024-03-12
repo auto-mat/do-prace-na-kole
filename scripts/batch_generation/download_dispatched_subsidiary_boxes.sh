b858cb194c61b569ab7dd7b22f57aa06a17dc5a8
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-03-12 09:57:47 +0100

    scripts/batch_generation: fix downloading dispatches subsidiary boxes from the GLS URL
    - `$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_FROM` env var must be actual
    campaign year beacuse the server return 504 Gateway Timeout Error in
    the case when we want filter data from date e.g. 01.01.2019 to date
    12.03.2024. `$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_FROM` and
    `$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO` must be setted in k8
    config maps file.
    
    - curl must follow redirect (-L flag) for downloading CSV file

--- scripts/batch_generation/download_dispatched_subsidiary_boxes.sh
+++ scripts/batch_generation/download_dispatched_subsidiary_boxes.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-curl "http://online.gls-czech.com/login.php" -c cookies.txt -b cookies.txt | grep script
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "username=$username&password=$password&lessersecurity=on" \
+     -c cookies.txt \
+     "$GLS_BASE_URL/login.php"
 
-if [ $? -ne 0 ] && [ -z "$username" ];  then
-   read -sp 'Enter password: ' password
-   echo -e "\n"
-   read -p 'Enter username: ' username
-   echo -e "\n"
+dateFrom=$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_FROM
+if [ "$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO" == "date +%d.%m.%Y" ]; then
+    dateTo=$($GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO)
+else
+    dateTo=$GLS_DISPATCHED_SUBSIDIARY_BOXES_DATE_TO
 fi
 
-dateFrom="01.01.2019"
-dateTo=`date +%d.%m.%Y`
-
-curl "http://online.gls-czech.com/login.php" -d "username=$username" -d "password=$password" -d "lessersecurity=on"  -c cookies.txt -b cookies.txt
-
-file=`curl "http://online.gls-czech.com/ll_getReportClass.php?uid=10424&lng=cz&fileType=CSV&dateFrom=$dateFrom&dateTo=$dateTo&senderID=-1&replist=0-30&gapid=-1" -c cookies.txt -b cookies.txt`
-echo $file
+file=$(curl -b cookies.txt "$GLS_BASE_URL/ll_getReportClass.php?uid=10424&lng=cz&fileType=CSV&dateFrom=$dateFrom&dateTo=$dateTo&senderID=-1&replist=0-30&gapid=-1")
 echo "carrier_identification,id,dispatched" > dispatched_subsidiary_boxes.csv
-curl "http://online.gls-czech.com/$file" -c cookies.txt -b cookies.txt > gls_data.csv
+echo "$GLS_BASE_URL/$file"
+curl -b cookies.txt -L "$GLS_BASE_URL/$file" > gls_data.csv
 tail gls_data.csv -n+2 | tr -d '\r' | sed 's/"//g' | cut -d ";" -f 1,3,5 | sed 's/;[^;][^;]*$/;1/' | tr ";" , >> dispatched_subsidiary_boxes.csv
 grep -P ",1\$|carrier" > dispatched_subsidiary_boxes_only_dispatched.csv < dispatched_subsidiary_boxes.csv
