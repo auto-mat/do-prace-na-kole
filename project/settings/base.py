cc211a4368e8a45c55eb3315b4ec225e8874290e
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-09-25 15:51:37 +0200

    project/settings: enable http://93.99.190.145:9000 app CORS origin

--- project/settings/base.py
+++ project/settings/base.py
@@ -484,6 +484,7 @@ LEAFLET_CONFIG = {
 CORS_ORIGIN_REGEX = [
     r"^(https?://)?(\w+\.)?dopracenakole\.(cz|net)$",
     "https://rtwbb-test.dopracenakole.net",
+    "http://93.99.190.145:9000",
 ]
 
 CORS_ORIGIN_REGEX_WHITELIST = CORS_ORIGIN_REGEX
