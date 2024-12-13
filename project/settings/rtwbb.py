a34a6597a35e97ababc65685b53035b7b72cdbb7
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-12-13 11:50:49 +0100

    project/settings: fix Python Black formatter error

--- project/settings/rtwbb.py
+++ project/settings/rtwbb.py
@@ -3,4 +3,6 @@ from .base import *  # noqa
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = "https://rtwbb-test.dopracenakole.net/#/"
+ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = (
+    "https://rtwbb-test.dopracenakole.net/#/"
+)
