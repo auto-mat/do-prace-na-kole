8ede85b48e0d04fdab32e27c2e0d1a375d0807d3
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-12-24 19:17:12 +0100

    apps/dpnk: fix Python Black formatter error

--- apps/dpnk/rest_permissions.py
+++ apps/dpnk/rest_permissions.py
@@ -8,17 +8,12 @@ class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if (
-                request.method in permissions.SAFE_METHODS
-                and "register-challenge" not in request.build_absolute_uri()
+            request.method in permissions.SAFE_METHODS
+            and "register-challenge" not in request.build_absolute_uri()
         ):
             return True
 
-        if view.action in (
-                "retrieve",
-                "list",
-                "partial_update",
-                "update"
-        ):
+        if view.action in ("retrieve", "list", "partial_update", "update"):
             return obj.user == request.user
 
         return True
