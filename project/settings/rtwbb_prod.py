95abafd17b4a29caf2baaca7ca0365f6ef71522c
Tomas Zigo <tomas.zigo@slovanet.sk>
2025-01-01 06:14:36 +0100

    project/settings: add rtwbb_prod.py settings file

---
+++ project/settings/rtwbb_prod.py
@@ -0,0 +1,9 @@
+from .base import *  # noqa
+
+
+ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = (
+    "https://rtwbb-test.dopracenakole.net/#/"
+)
+
+ACCOUNT_EMAIL_CONFIRMATION_URL = "https://rtwbb-test.dopracenakole.net/#/confirm-email"
+ACCOUNT_ADAPTER = "dpnk.allauth.AccountAdapter"
