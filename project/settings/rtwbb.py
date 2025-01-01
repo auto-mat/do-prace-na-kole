3bcf7ca26a42056efa458055de430e4d5c46c1e9
michalklimpera <79811508+michalklimpera@users.noreply.github.com>
2025-01-01 06:02:06 +0100

    apps/dpnk: add support for external email confirmation page (#362)
    * apps/dpnk: add support for external email confirmation page
    
    * project/settings: move confirmation email settings into rtwbb.py
    
    * project/settings: fix formatting in rtwbb.py

--- project/settings/rtwbb.py
+++ project/settings/rtwbb.py
@@ -6,3 +6,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = (
     "https://rtwbb-test.dopracenakole.net/#/"
 )
+
+ACCOUNT_EMAIL_CONFIRMATION_URL = "https://rtwbb-test.dopracenakole.net/#/confirm-email"
+ACCOUNT_ADAPTER = "dpnk.allauth.AccountAdapter"
