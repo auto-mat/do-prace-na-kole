ec2600d66316069b29c5a37e79a5cde88beefd5d
Tomas Zigo <tomas.zigo@slovanet.sk>
2025-02-05 17:16:52 +0100

    apps/dpnk: add/use custom allauth lib SocialAccountAdapter() adapter class
    Which allowing login existed user account (login with email/password)
    with social media login with same email address.

--- apps/dpnk/allauth.py
+++ apps/dpnk/allauth.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.utils.http import urlencode
 
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.account.utils import user_pk_to_url_str
 from dj_rest_auth.serializers import PasswordResetSerializer
 
@@ -29,3 +31,44 @@ class UserPasswordResetSerializer(PasswordResetSerializer):
 
     def get_email_options(self):
         return {"url_generator": self._reset_pass_url_generator}
+
+
+class SocialAccountAdapter(DefaultSocialAccountAdapter):
+    """Allow login existed user account (login with email/password) with
+    social media login with same email address
+
+    https://github.com/pennersr/django-allauth/issues/418#issuecomment-107880925
+    """
+
+    def pre_social_login(self, request, sociallogin):
+        # Social account already exists, so this is just a login
+        if sociallogin.is_existing:
+            return
+
+        # Some social logins don't have an email address
+        if not sociallogin.email_addresses:
+            return
+
+        # Find the first verified email that we get from this sociallogin
+        verified_email = None
+        for email in sociallogin.email_addresses:
+            if email.verified:
+                verified_email = email
+                break
+
+        # No verified emails found, nothing more to do
+        if not verified_email:
+            return
+
+        # Check if given email address already exists as a verified email on
+        # an existing user's account
+        try:
+            existing_email = EmailAddress.objects.get(
+                email__iexact=email.email,
+                verified=True,
+            )
+        except EmailAddress.DoesNotExist:
+            return
+
+        # If it does, connect this new social login to the existing user
+        sociallogin.connect(request, existing_email.user)
