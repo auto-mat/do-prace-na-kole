fe98576fbe7348c53ed758ad1525fe7e21a2f794
Tomas Zigo <tomas.zigo@slovanet.sk>
2025-10-13 13:17:26 +0200

    project/settings: fix allowing setting RTWBB_FRONTEND_APP_BASE_URL ENV multiple value

--- project/settings/rtwbb_test.py
+++ project/settings/rtwbb_test.py
@@ -6,6 +6,22 @@ RTWBB_FRONTEND_APP_BASE_URL = os.getenv(
     "https://rtwbb-test.dopracenakole.net/#/",
 )
 
+rtwbb_frontend_app_base_url_split_char = ","
+rtwbb_frontend_app_base_url_fragment_identifier = "/#/"
+if rtwbb_frontend_app_base_url_split_char in RTWBB_FRONTEND_APP_BASE_URL:
+    urls = RTWBB_FRONTEND_APP_BASE_URL.split(rtwbb_frontend_app_base_url_split_char)
+    for url in urls:
+        CORS_ORIGIN_REGEX.append(
+            url.rstrip(rtwbb_frontend_app_base_url_fragment_identifier)
+        )
+    RTWBB_FRONTEND_APP_BASE_URL = urls[0]
+else:
+    CORS_ORIGIN_REGEX.append(
+        RTWBB_FRONTEND_APP_BASE_URL.rstrip(
+            rtwbb_frontend_app_base_url_fragment_identifier
+        )
+    )
+
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = RTWBB_FRONTEND_APP_BASE_URL
 
 ACCOUNT_EMAIL_CONFIRMATION_URL = f"{RTWBB_FRONTEND_APP_BASE_URL}confirm-email"
@@ -17,20 +33,3 @@ ACCOUNT_ADAPTER = "dpnk.allauth.AccountAdapter"
 SOCIALACCOUNT_ADAPTER = "dpnk.allauth.SocialAccountAdapter"
 
 REST_AUTH["PASSWORD_RESET_SERIALIZER"] = "dpnk.allauth.UserPasswordResetSerializer"
-
-
-rtwbb_frontend_app_base_url_split_char = ","
-rtwbb_frontend_app_base_url_fragment_identifier = "/#/"
-if rtwbb_frontend_app_base_url_split_char in RTWBB_FRONTEND_APP_BASE_URL:
-    for url in RTWBB_FRONTEND_APP_BASE_URL.split(
-        rtwbb_frontend_app_base_url_split_char
-    ):
-        CORS_ORIGIN_REGEX.append(
-            url.rstrip(rtwbb_frontend_app_base_url_fragment_identifier)
-        )
-else:
-    CORS_ORIGIN_REGEX.append(
-        RTWBB_FRONTEND_APP_BASE_URL.rstrip(
-            rtwbb_frontend_app_base_url_fragment_identifier
-        )
-    )
