c524f576e132a4ad7209be699da6e43c2915338c
Tomas Zigo <tomas.zigo@slovanet.sk>
2026-06-18 07:43:06 +0200

    apps/dpnk: replace removed ungettext_lazy() func with gettext_lazy() func
    Compatibile with Django 5.2 version.

--- apps/dpnk/models/competition.py
+++ apps/dpnk/models/competition.py
@@ -28,7 +28,6 @@ from django.dispatch import receiver
 from django.utils.html import escape
 from django.utils.text import format_lazy
 from django.utils.translation import gettext_lazy as _
-from django.utils.translation import ungettext_lazy
 
 from rank import Rank, UpperRank
 
@@ -507,7 +506,7 @@ class Competition(models.Model):
 
         cities = self.city.all()
         if cities:
-            city_string = ungettext_lazy(
+            city_string = _(
                 "ve městě %(cities)s",
                 "ve městech %(cities)s",
                 len(cities),
